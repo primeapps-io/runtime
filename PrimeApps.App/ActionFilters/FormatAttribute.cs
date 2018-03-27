ee808e42d4360848a20a3be060e34b84525f8f4c
Taner Özel <taner.ozel@ofisim.com>
2018-03-27 16:53:12 +0300

    #controller

--- PrimeApps.App/ActionFilters/FormatAttribute.cs
+++ PrimeApps.App/ActionFilters/FormatAttribute.cs
@@ -1,34 +1,10 @@
-﻿using System;
-using System.Net.Http;
-using System.Net.Http.Formatting;
-using System.Threading;
-using System.Threading.Tasks;
-using System.Web.Http.Controllers;
-using System.Web.Http.Filters;
-using Newtonsoft.Json;
+﻿
+using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace PrimeApps.App.ActionFilters
 {
     public class FormatAttribute : ActionFilterAttribute
-    {
-        public Task<HttpResponseMessage> ExecuteActionFilterAsync(HttpActionContext actionContext, CancellationToken cancellationToken, Func<Task<HttpResponseMessage>> continuation)
-        {
-            var formatter = new JsonMediaTypeFormatter
-            {
-                SerializerSettings =
-                {
-                    PreserveReferencesHandling = PreserveReferencesHandling.None,
-                    ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
-                    NullValueHandling = NullValueHandling.Ignore,
-                    DateParseHandling = DateParseHandling.None,
-                    DateTimeZoneHandling = DateTimeZoneHandling.Utc
-                }
-            };
+    { 
 
-            actionContext.RequestContext.Configuration.Formatters.Clear();
-            actionContext.RequestContext.Configuration.Formatters.Add(formatter);
-
-            return continuation();
-        }
     }
 }
