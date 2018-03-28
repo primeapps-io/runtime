61e852d4938035ec0c29a52fa7d9935c3d3e46fb
Bahadır Civelek <bahadir.civelek@ofisim.com>
2018-03-28 10:53:57 +0300

    TENANTDBCONTEXT

---
+++ PrimeApps.Model/Entities/Application/AnalyticTenantUser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrimeApps.Model.Entities.Application
+{
+    public class AnalyticTenantUser
+    {
+	    public int AnaltyicId { get; set; }
+	    public Analytic Analytic { get; set; }
+
+	    public int TenantUserId { get; set; }
+	    public TenantUser TenantUser { get; set; }
+	}
+}
---
+++ PrimeApps.Model/Entities/Application/NoteTenantUser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrimeApps.Model.Entities.Application
+{
+    public class NoteTenantUser
+    {
+	    public int NoteId { get; set; }
+	    public Note Note { get; set; }
+
+	    public int TenantUserId { get; set; }
+	    public TenantUser TenantUser { get; set; }
+	}
+}
