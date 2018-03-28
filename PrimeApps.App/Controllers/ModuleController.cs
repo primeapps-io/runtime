b76c06f878f6aaafc993b19ab4a0906c11f5d752
Atakan Gemici <atakan.gemici@ofisim.com>
2018-03-28 13:35:15 +0300

    Controller Bugfix

--- PrimeApps.App/Controllers/ModuleController.cs
+++ PrimeApps.App/Controllers/ModuleController.cs
@@ -281,7 +281,8 @@ namespace PrimeApps.App.Controllers
                     if (resultCreateJunctionTable != -1)
                     {
                         await _moduleRepository.DeleteRelationHard(relationEntity);
-                        throw new HttpResponseException(HttpStatusCode.Status500InternalServerError);
+
+                        throw new ApplicationException(HttpStatusCode.Status500InternalServerError.ToString());
 
                         //throw new HttpResponseException(HttpStatusCode.Status500InternalServerError);
                     }
@@ -347,7 +348,7 @@ namespace PrimeApps.App.Controllers
             var resultCreate = await _moduleRepository.CreateDependency(dependencyEntity);
 
             if (resultCreate < 1)
-                throw new HttpResponseException(HttpStatusCode.Status500InternalServerError);
+                throw new ApplicationException(HttpStatusCode.Status500InternalServerError.ToString());
 
             //throw new HttpResponseException(HttpStatusCode.Status500InternalServerError);
 
