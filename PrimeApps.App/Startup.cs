7b57bf7eadb2e34fa2f86d7eede4722a09415d75
İbrahim Varol <ibrahim.varol@ofisim.com>
2018-04-18 17:34:20 +0200

    Added MVC and Data Annotation Localizations.

--- PrimeApps.App/Startup.cs
+++ PrimeApps.App/Startup.cs
@@ -15,56 +15,56 @@ using System.Web;
 namespace PrimeApps.App
 {
     public partial class Startup
-	{
-		public IHostingEnvironment HostingEnvironment { get; }
-		public IConfiguration Configuration { get; }
+    {
+        public IHostingEnvironment HostingEnvironment { get; }
+        public IConfiguration Configuration { get; }
 
-		public static string PublicClientId { get; private set; }
+        public static string PublicClientId { get; private set; }
 
-		// This method gets called by the runtime. Use this method to add services to the container.
-		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
 
-		public Startup(IHostingEnvironment env)
-		{
-			PublicClientId = "self";
+        public Startup(IHostingEnvironment env)
+        {
+            PublicClientId = "self";
 
 
-			var builder = new ConfigurationBuilder()
-				.SetBasePath(env.ContentRootPath)
-				.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-				.AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
-				.AddEnvironmentVariables()
-				.Build();
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables()
+                .Build();
 
-			Configuration = builder;
-			HostingEnvironment = env;
+            Configuration = builder;
+            HostingEnvironment = env;
 
-		}
+        }
 
-		public void ConfigureServices(IServiceCollection services)
-		{
-			#region IdentityServer4
-			services.AddMvcCore()
-				.AddAuthorization()
-				.AddJsonFormatters();
+        public void ConfigureServices(IServiceCollection services)
+        {
+            #region IdentityServer4
+            services.AddMvcCore()
+                .AddAuthorization()
+                .AddJsonFormatters();
 
-			services.AddAuthentication("Bearer")
-				.AddIdentityServerAuthentication(options =>
-				{
-					options.Authority = "http://localhost:51735";
-					options.RequireHttpsMetadata = false;
-					options.ApiName = "api1";
-				}); 
-			#endregion
+            services.AddAuthentication("Bearer")
+                .AddIdentityServerAuthentication(options =>
+                {
+                    options.Authority = "http://localhost:51735";
+                    options.RequireHttpsMetadata = false;
+                    options.ApiName = "api1";
+                });
+            #endregion
 
-			/*services.AddIdentity<PlatformUser, ApplicationRole>()
+            /*services.AddIdentity<PlatformUser, ApplicationRole>()
 			   .AddUserStore<PlatformDBContext>()
 			   .AddUserManager<ApplicationUserManager>()
 			   .AddRoleManager<ApplicationUserRole>()
 			   .AddEntityFrameworkStores<PlatformDBContext, int>()
 			   .AddDefaultTokenProviders();*/
 
-			/*services.AddIdentity<ApplicationUserManager, ApplicationUserRole>(options =>
+            /*services.AddIdentity<ApplicationUserManager, ApplicationUserRole>(options =>
 			{
 				options.Password.RequiredLength = 10;
 				options.Password.RequireLowercase = false;
@@ -76,32 +76,32 @@ namespace PrimeApps.App
 				options.User.AllowedUserNameCharacters = "";
 			});*/
 
-			services.Configure<MvcOptions>(options =>
-			{
-				options.Filters.Add(new RequireHttpsAttribute());
-			});
+            services.Configure<MvcOptions>(options =>
+            {
+                options.Filters.Add(new RequireHttpsAttribute());
+            });
 
-			services.AddLocalization(o => o.ResourcesPath = "Localization");
-	        services.Configure<RequestLocalizationOptions>(options =>
-	        {
-		        var supportedCultures = new[]
-		        {
-			        new CultureInfo("en-US"),
-			        new CultureInfo("tr-TR")
-		        };
-		        options.DefaultRequestCulture = new RequestCulture("tr-TR", "tr-TR");
+            services.AddLocalization(o => o.ResourcesPath = "Localization");
+            services.Configure<RequestLocalizationOptions>(options =>
+            {
+                var supportedCultures = new[]
+                {
+                    new CultureInfo("en-US"),
+                    new CultureInfo("tr-TR")
+                };
+                options.DefaultRequestCulture = new RequestCulture("tr-TR", "tr-TR");
 
-		        // You must explicitly state which cultures your application supports.
-		        // These are the cultures the app supports for formatting 
-		        // numbers, dates, etc.
+                // You must explicitly state which cultures your application supports.
+                // These are the cultures the app supports for formatting 
+                // numbers, dates, etc.
 
-		        options.SupportedCultures = supportedCultures;
+                options.SupportedCultures = supportedCultures;
 
-		        // These are the cultures the app supports for UI strings, 
-		        // i.e. we have localized resources for.
+                // These are the cultures the app supports for UI strings, 
+                // i.e. we have localized resources for.
 
-		        options.SupportedUICultures = supportedCultures;
-	        });
+                options.SupportedUICultures = supportedCultures;
+            });
 
             // Add application services. DI
             //services.AddTransient<IProductRepository, ProductRepository>();
@@ -113,7 +113,7 @@ namespace PrimeApps.App
 
             RegisterAuth(services);
 
-			/*services.AddMvc(options =>
+            /*services.AddMvc(options =>
             {
 
                 options.CacheProfiles.Add("Nocache",
@@ -123,108 +123,111 @@ namespace PrimeApps.App
                         NoStore = true,
                     });
             });*/
-			services.AddCors(options =>
-			{
-				options.AddPolicy("AllowAll",
-					builder =>
-					{
-						builder
-							.AllowAnyOrigin()
-							.AllowAnyMethod()
-							.AllowAnyHeader()
-							.AllowCredentials();
-					});
-			});
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAll",
+                    builder =>
+                    {
+                        builder
+                            .AllowAnyOrigin()
+                            .AllowAnyMethod()
+                            .AllowAnyHeader()
+                            .AllowCredentials();
+                    });
+            });
 
             services.AddMvc()
-				.SetCompatibilityVersion(CompatibilityVersion.Version_2_1)
-				.AddJsonOptions(opt =>
-				{
-					#region SnakeCaseAttribute
-					opt.SerializerSettings.ContractResolver = new DefaultContractResolver()
-					{
-						NamingStrategy = new SnakeCaseNamingStrategy()
-					}; 
-					#endregion
-				});
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_1)
+                .AddJsonOptions(opt =>
+                {
+                    #region SnakeCaseAttribute
+                    opt.SerializerSettings.ContractResolver = new DefaultContractResolver()
+                    {
+                        NamingStrategy = new SnakeCaseNamingStrategy()
+                    };
+                    #endregion
+                }).AddViewLocalization(
+                LanguageViewLocationExpanderFormat.Suffix,
+                opts => { opts.ResourcesPath = "Localization"; })
+                .AddDataAnnotationsLocalization();
 
-			#region RequireHttpsAttribute
-			services.Configure<MvcOptions>(options =>
-				{
-					options.Filters.Add(new RequireHttps());
-				});
-			#endregion
-			
-
-			RegisterBundle(services);
-		}
-
-		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-		{
-			app.Use(async (context, next) =>
-			{
-				if (context.Request.QueryString.HasValue)
-				{
-					if (!string.IsNullOrWhiteSpace(context.Request.Headers["Authorization"]))
-					{
-						var queryString = HttpUtility.ParseQueryString(context.Request.QueryString.Value);
-						string token = queryString.Get("access_token");
-
-						if (!string.IsNullOrWhiteSpace(token))
-						{
-							context.Request.Headers.Add("Authorization", new[] { string.Format("Bearer {0}", token) });
-						}
-					}
-				}
-				// Call the next delegate/middleware in the pipeline
-				await next.Invoke();
-			});
+            #region RequireHttpsAttribute
+            services.Configure<MvcOptions>(options =>
+                {
+                    options.Filters.Add(new RequireHttps());
+                });
+            #endregion
 
 
-			app.UseAuthentication();
+            RegisterBundle(services);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.Use(async (context, next) =>
+            {
+                if (context.Request.QueryString.HasValue)
+                {
+                    if (!string.IsNullOrWhiteSpace(context.Request.Headers["Authorization"]))
+                    {
+                        var queryString = HttpUtility.ParseQueryString(context.Request.QueryString.Value);
+                        string token = queryString.Get("access_token");
+
+                        if (!string.IsNullOrWhiteSpace(token))
+                        {
+                            context.Request.Headers.Add("Authorization", new[] { string.Format("Bearer {0}", token) });
+                        }
+                    }
+                }
+                // Call the next delegate/middleware in the pipeline
+                await next.Invoke();
+            });
 
 
-			BundleConfiguration(app);
+            app.UseAuthentication();
 
-			if (env.IsDevelopment())
-			{
-				app.UseDeveloperExceptionPage();
 
-				//app.UseDatabaseErrorPage();
-				//app.UseBrowserLink();
-			}
-			else
-			{
-				//app.UseExceptionHandler("/Home/Error");
-			}
-			/// Must always stay before static files middleware.
-			app.UseHttpsRedirection();
+            BundleConfiguration(app);
 
-			/*
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+
+                //app.UseDatabaseErrorPage();
+                //app.UseBrowserLink();
+            }
+            else
+            {
+                //app.UseExceptionHandler("/Home/Error");
+            }
+            /// Must always stay before static files middleware.
+            app.UseHttpsRedirection();
+
+            /*
 			 * In ASP.NET, static files are stored in various directories and referenced in the views.
 			 * In ASP.NET Core, static files are stored in the "web root" (<content root>/wwwroot), unless configured otherwise.
 			 * The files are loaded into the request pipeline by invoking the UseStaticFiles
 			 */
-			app.UseStaticFiles();
+            app.UseStaticFiles();
 
-			//app.UseIdentity();
+            //app.UseIdentity();
 
-			app.UseCors("AllowAll");
+            app.UseCors("AllowAll");
 
-			JobConfiguration(app);
-			app.UseMvc(routes =>
-			{
-				routes.MapRoute(
-					name: "default",
-					template: "{controller=Auth}/{action=Login}/{id?}");
-			});
+            JobConfiguration(app);
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Auth}/{action=Login}/{id?}");
+            });
 
-			/*app.Run(async (context) =>
+            /*app.Run(async (context) =>
             {
                 await context.Response.WriteAsync("Hello World!");
             });*/
-			app.UseHsts();
-		}
-	}
+            app.UseHsts();
+        }
+    }
 }
