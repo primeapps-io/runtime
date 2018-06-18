41669d840f5069fa5ca491865f8319d28f4a570d
Bahadır Civelek <bahadir.civelek@ofisim.com>
2018-06-18 09:14:53 +0300

    login

---
+++ PrimeApps.App/Models/ActivateBindingModels.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+
+namespace PrimeApps.App.Models
+{
+	public class ActivateBindingModels
+	{
+		[DataMember(Name = "email"), Required]
+		public string Email { get; set; }
+
+		[DataMember(Name = "app_id"), Required]
+		public int AppId { get; set; }
+
+		[DataMember(Name = "culture")]
+		public string Culture { get; set; }
+
+		[DataMember(Name = "first_name")]
+		public string FirstName { get; set; }
+
+		[DataMember(Name = "email_confirmed")]
+		public bool EmailConfirmed { get; set; }
+
+		[DataMember(Name = "last_name")]
+		public string LastName { get; set; }
+
+		[DataMember(Name = "token")]
+		public string Token { get; set; }
+	}
+}
---
+++ PrimeApps.Auth/Models/UserViewModels/ActivateBindingModels.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrimeApps.Auth.Models.UserViewModels
+{
+	public class ActivateBindingModels
+	{
+		[Required]
+		public string email { get; set; }
+
+		[Required]
+		public int app_id { get; set; }
+		public string culture { get; set; }
+		public string first_name { get; set; }
+		public bool email_confirmed { get; set; }
+		public string last_name { get; set; }
+		public string token { get; set; }
+	}
+}
