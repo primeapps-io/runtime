990ee611993d6779be8c84c274b8d39e6afdf2ec
Galip Çevrik <galip.cevrik@ofisim.com>
2018-07-04 17:37:14 +0300

    cache entity eklendi.

---
+++ PrimeApps.Model/Migrations/PlatformDB/20180704130624_Task2170.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PrimeApps.Model.Migrations.PlatformDB
+{
+    public partial class Task2170 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "cache",
+                schema: "public",
+                columns: table => new
+                {
+                    key = table.Column<string>(maxLength: 100, nullable: false),
+                    value = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_cache", x => x.key);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_cache_key",
+                schema: "public",
+                table: "cache",
+                column: "key");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "cache",
+                schema: "public");
+        }
+    }
+}
