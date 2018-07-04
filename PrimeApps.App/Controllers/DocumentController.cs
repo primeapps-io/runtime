7d335922e791c0eb6ea9b7a44a355146a2facdd0
İbrahim Varol <ibrahim.varol@ofisim.com>
2018-07-04 14:30:41 +0200

    Excel upload/download fixes.

--- PrimeApps.App/Controllers/DocumentController.cs
+++ PrimeApps.App/Controllers/DocumentController.cs
@@ -104,9 +104,8 @@ namespace PrimeApps.App.Controllers
         [Route("Upload_Excel"), HttpPost]
         public async Task<IActionResult> UploadExcel()
         {
-            var requestStream = await Request.ReadAsStreamAsync();
             DocumentUploadResult result;
-            var isUploaded = _documentHelper.UploadExcel(requestStream, out result);
+            var isUploaded = _documentHelper.UploadExcel(Request.Body, out result);
 
             if (!isUploaded && result == null)
             {
