133ad9f8c02709a3a1512d3aadf998cc64566d7f
Fatih Sever <fatih.sever@ofisim.com>
2018-09-27 22:38:37 +0300

    Migration TenantUser entity'sine Phone alanı eklendi

---
+++ PrimeApps.Model/Migrations/TenantDB/20180927123037_Task2262.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PrimeApps.Model.Migrations.TenantDB
+{
+    public partial class Task2262 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "phone",
+                schema: "public",
+                table: "users",
+                maxLength: 50,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "phone",
+                schema: "public",
+                table: "users");
+        }
+    }
+}
