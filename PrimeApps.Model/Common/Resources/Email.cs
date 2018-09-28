d9d427360e9d3deafc6ceb32056f392c724a6356
Onur Bostancıoğlu <onur.bostancioglu@ofisim.com>
2018-09-28 13:07:13 +0300

    Update Enum EmailResource

--- PrimeApps.Model/Common/Resources/Email.cs
+++ PrimeApps.Model/Common/Resources/Email.cs
@@ -7,51 +7,58 @@ namespace PrimeApps.Model.Common.Resources
 {
     public enum EmailResource
     {
-        [ResourceName("email-reset")]
+        [ResourceName("email_reset")]
         EmailReset,
-        [ResourceName("email-status-successful")]
+        [ResourceName("email_status_successful")]
         EMailStatusSuccessful,
 
-        [ResourceName("email-status-failed")]
+        [ResourceName("email_status_failed")]
         EMailStatusFailed,
 
-        [ResourceName("event-reminder")]
+        [ResourceName("event_reminder")]
         EventReminder,
 
-        [ResourceName("task-reminder")]
+        [ResourceName("task_reminder")]
         TaskReminder,
 
-        [ResourceName("call-reminder")]
+        [ResourceName("call_reminder")]
         CallReminder,
 
-        [ResourceName("task-owner-changed-notification")]
+        [ResourceName("task_owner_changed_notification")]
         TaskOwnerChangedNotification,
 
-        [ResourceName("owner-changed-notification")]
+        [ResourceName("owner_changed_notification")]
         OwnerChangedNotification,
 
-        [ResourceName("task-assigned-notification")]
+        [ResourceName("task_assigned_notification")]
         TaskAssignedNotification,
 
-        [ResourceName("approval-process-create-notification")]
+        [ResourceName("approval_process_create_notification")]
         ApprovalProcessCreateNotification,
 
-        [ResourceName("approval-process-update-notification")]
+        [ResourceName("approval_process_update_notification")]
         ApprovalProcessUpdateNotification,
 
-        [ResourceName("approval-process-reject-notification")]
+        [ResourceName("approval_process_reject_notification")]
         ApprovalProcessRejectNotification,
 
-        [ResourceName("approval-process-update-reject-notification")]
+        [ResourceName("approval_process_update_reject_notification")]
         ApprovalProcessUpdateRejectNotification,
 
-        [ResourceName("approval-process-approve-notification")]
+        [ResourceName("approval_process_approve_notification")]
         ApprovalProcessApproveNotification,
 
-        [ResourceName("workflow-notification")]
+        [ResourceName("workflow_notification")]
         WorkflowNotification,
 
-        [ResourceName("trial-expire-mail")]
-        TrialExpireMail
+        [ResourceName("trial_expire_mail")]
+        TrialExpireMail,
+
+        [ResourceName("sms_status_failed")]
+        SmsStatusFailed,
+
+        [ResourceName("sms_status_successful")]
+        SmsStatusSuccessful,
+
     }
 }
