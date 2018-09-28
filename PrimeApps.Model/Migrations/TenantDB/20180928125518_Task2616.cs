f0aa74439f5c81f1db17c6b1373294f9e5b54a5d
Fatih Sever <fatih.sever@ofisim.com>
2018-09-28 15:55:58 +0300

    #2616 Email şablonlarının Tenant'a özel olması

---
+++ PrimeApps.Model/Migrations/TenantDB/20180928125518_Task2616.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PrimeApps.Model.Migrations.TenantDB
+{
+    public partial class Task2616 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "code",
+                schema: "public",
+                table: "templates",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_templates_id_code",
+                schema: "public",
+                table: "templates",
+                columns: new[] { "id", "code" },
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_templates_id_code",
+                schema: "public",
+                table: "templates");
+
+            migrationBuilder.DropColumn(
+                name: "code",
+                schema: "public",
+                table: "templates");
+        }
+    }
+}
