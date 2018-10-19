935d29ed285eb213d5e0bc23df251c51f3d2f402
Fatih Sever <fatih.sever@ofisim.com>
2018-10-19 15:19:32 +0300

    #2742 AccountCleanup Job'ı çalışmıyor

--- PrimeApps.App/Jobs/AccountCleanup.cs
+++ PrimeApps.App/Jobs/AccountCleanup.cs
@@ -1,6 +1,4 @@
-﻿using Npgsql;
-using PrimeApps.Model.Context;
-using PrimeApps.Model.Helpers;
+﻿using PrimeApps.Model.Context;
 using PrimeApps.Model.Repositories;
 using System;
 using System.Collections.Generic;
@@ -9,60 +7,45 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using PrimeApps.App.Helpers;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.AspNetCore.Http;
 
 namespace PrimeApps.App.Jobs
 {
-	/// Drops tenant databases that are inactive more then 1 month from servers.
-	public class AccountCleanup
-	{
-		private IConfiguration _configuration;
-		private IServiceScopeFactory _serviceScopeFactory;
+    public class AccountCleanup
+    {
+        private IConfiguration _configuration;
+        private IServiceScopeFactory _serviceScopeFactory;
 
-		public AccountCleanup(IConfiguration configuration, IServiceScopeFactory serviceScopeFactory)
-		{
-			_configuration = configuration;
-			_serviceScopeFactory = serviceScopeFactory;
-		}
+        public AccountCleanup(IConfiguration configuration, IServiceScopeFactory serviceScopeFactory)
+        {
+            _configuration = configuration;
+            _serviceScopeFactory = serviceScopeFactory;
+        }
 
-		/// <summary>
-		/// Execute the job
-		/// </summary>
-		public async Task Run()
-		{
-			IList<int> expiredTenants = new List<int>();
-			var connectionString = "";
+        public async Task Run()
+        {
+            using (var scope = _serviceScopeFactory.CreateScope())
+            {
+                IList<int> expiredTenants;
+                var platformDBContext = scope.ServiceProvider.GetRequiredService<PlatformDBContext>();
 
-			using (var scope = _serviceScopeFactory.CreateScope())
-			{
-				var platformDBContext = scope.ServiceProvider.GetRequiredService<PlatformDBContext>();
-				using (var tenantRepository = new TenantRepository(platformDBContext, _configuration))
-				{
-					// Get expired inactive tenant ids.
-					expiredTenants = await tenantRepository.GetExpiredTenantIdsToDelete();
-					connectionString = platformDBContext.Database.GetDbConnection().ConnectionString;
-				}
+                using (var tenantRepository = new TenantRepository(platformDBContext, _configuration))
+                {
+                    expiredTenants = await tenantRepository.GetExpiredTenantIdsToDelete();
+                }
 
-
-
-				foreach (var tenantId in expiredTenants)
-				{
-					var dropSql = "DROP DATABASE IF EXISTS tenant" + tenantId+";";
-					try
-					{
-						platformDBContext.Database.ExecuteSqlCommand(dropSql);
-
-					}
-					catch (Exception ex)
-					{
-						// the query will fail in any unexpected case rather then non-existing database, and it will be logged here with the details. 
-						//Error err = new Error(ex);
-						//err.Detail = $"Expired tenant {tenantId} could not be removed by Account Cleanup Job";
-						//ErrorLog.GetDefault(null).Log(err);
-						ErrorHandler.LogError(ex, $"Expired tenant {tenantId} could not be removed by Account Cleanup Job" + " " + "tenant_id:" + tenantId);
-					}
-				}
-			}
-		}
-	}
+                foreach (var tenantId in expiredTenants)
+                {
+                    var dropSql = "DROP DATABASE IF EXISTS tenant" + tenantId + ";";
+                    try
+                    {
+                        platformDBContext.Database.ExecuteSqlCommand(dropSql);
+                    }
+                    catch (Exception ex)
+                    {
+                        ErrorHandler.LogError(ex, $"Expired tenant {tenantId} could not be removed by Account Cleanup Job" + " " + "tenant_id:" + tenantId);
+                    }
+                }
+            }
+        }
+    }
 }
--- PrimeApps.App/Jobs/AccountDeactivate.cs
+++ PrimeApps.App/Jobs/AccountDeactivate.cs
@@ -1,4 +1,5 @@
-﻿using PrimeApps.Model.Context;
+﻿using System;
+using PrimeApps.Model.Context;
 using System.Threading.Tasks;
 using Npgsql;
 using PrimeApps.Model.Helpers.QueryTranslation;
@@ -6,65 +7,66 @@ using PrimeApps.Model.Repositories;
 using System.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.AspNetCore.Http;
 using PrimeApps.Model.Helpers;
-using PrimeApps.App.Helpers;
 
 namespace PrimeApps.App.Jobs
 {
-	public class AccountDeactivate
-	{
-		private IConfiguration _configuration;
-		private IServiceScopeFactory _serviceScopeFactory;
+    public class AccountDeactivate
+    {
+        private IConfiguration _configuration;
+        private IServiceScopeFactory _serviceScopeFactory;
 
-		public AccountDeactivate(IConfiguration configuration, IServiceScopeFactory serviceScopeFactory)
-		{
-			_configuration = configuration;
-			_serviceScopeFactory = serviceScopeFactory;
-		}
+        public AccountDeactivate(IConfiguration configuration, IServiceScopeFactory serviceScopeFactory)
+        {
+            _configuration = configuration;
+            _serviceScopeFactory = serviceScopeFactory;
+        }
 
-		public async Task Deactivate()
-		{
-			using (var scope = _serviceScopeFactory.CreateScope())
-			{
-				var databaseContext = scope.ServiceProvider.GetRequiredService<TenantDBContext>();
-				var platformDatabaseContext = scope.ServiceProvider.GetRequiredService<PlatformDBContext>();
-				using (var tenantRepository = new TenantRepository(platformDatabaseContext, _configuration))
-				using (var userRepository = new UserRepository(databaseContext, _configuration))
-				{
-					var tenants = await tenantRepository.GetExpiredTenants();
-					foreach (var tenant in tenants)
-					{
-						userRepository.CurrentUser = new CurrentUser { TenantId = tenant.Id, UserId = 1 };
-						var users = await userRepository.GetAllAsync();
+        public async Task Deactivate()
+        {
+            using (var scope = _serviceScopeFactory.CreateScope())
+            {
+                var databaseContext = scope.ServiceProvider.GetRequiredService<TenantDBContext>();
+                var platformDatabaseContext = scope.ServiceProvider.GetRequiredService<PlatformDBContext>();
 
-						foreach (var user in users)
-						{
-							try
-							{
-								user.IsActive = false;
-								databaseContext.SaveChanges();
-							}
-							//TODO: ex.InnerException.InnerException olabilir
-							catch (DataException ex)
-							{
-								if (ex.InnerException is PostgresException)
-								{
-									var innerEx = (PostgresException)ex.InnerException;
+                using (var tenantRepository = new TenantRepository(platformDatabaseContext, _configuration))
+                using (var userRepository = new UserRepository(databaseContext, _configuration))
+                {
+                    var tenants = await tenantRepository.GetExpiredTenants();
 
-									if (innerEx.SqlState == PostgreSqlStateCodes.DatabaseDoesNotExist)
-										continue;
-								}
+                    foreach (var tenant in tenants)
+                    {
+                        userRepository.CurrentUser = new CurrentUser { TenantId = tenant.Id, UserId = 1 };
+                        var users = await userRepository.GetAllAsync();
 
-								throw;
-							}
-						}
+                        foreach (var user in users)
+                        {
+                            try
+                            {
+                                user.IsActive = false;
+                                databaseContext.SaveChanges();
+                            }
+                            catch (DataException ex)
+                            {
+                                if (ex.InnerException is PostgresException)
+                                {
+                                    var innerEx = (PostgresException)ex.InnerException;
 
-						tenant.License.IsDeactivated = true;
-						await tenantRepository.UpdateAsync(tenant);
-					}
-				}
-			}
-		}
-	}
+                                    if (innerEx.SqlState == PostgreSqlStateCodes.DatabaseDoesNotExist)
+                                        continue;
+                                }
+
+                                throw;
+                            }
+                        }
+
+                        tenant.License.IsDeactivated = true;
+                        tenant.License.DeactivatedAt = DateTime.UtcNow;
+
+                        await tenantRepository.UpdateAsync(tenant);
+                    }
+                }
+            }
+        }
+    }
 }
