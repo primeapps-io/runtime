bba6950b774ef35d5c096de19b2cadafffd18bfd
Fatih Sever <fatih.sever@ofisim.com>
2018-10-23 01:48:52 +0300

    Bugfix

--- PrimeApps.Auth/Helpers/AuthHelper.cs
+++ PrimeApps.Auth/Helpers/AuthHelper.cs
@@ -24,7 +24,7 @@ namespace PrimeApps.Auth.UI
     {
         public static async Task<Application> GetApplicationInfoAsync(IConfiguration configuration, HttpRequest request, string returnUrl, IApplicationRepository applicationRepository)
         {
-            var _language = !string.IsNullOrEmpty(request.Cookies[".AspNetCore.Culture"]) ? request.Cookies[".AspNetCore.Culture"].Split("uic=")[1] : null;
+            var language = !string.IsNullOrEmpty(request.Cookies[".AspNetCore.Culture"]) ? request.Cookies[".AspNetCore.Culture"].Split("uic=")[1] : null;
 
             var cdnUrlStatic = "";
             var cdnUrl = configuration.GetSection("webOptimizer")["cdnUrl"];
@@ -36,46 +36,40 @@ namespace PrimeApps.Auth.UI
             }
 
             var clientId = GetClientId(returnUrl);
-
-            var primeLang = _language ?? "tr";
-
             var app = await applicationRepository.GetByName(clientId);
 
-            if (string.IsNullOrWhiteSpace(_language))
-                _language = app.Setting.Language ?? "tr";
+            if (string.IsNullOrWhiteSpace(language))
+                language = app.Setting.Language ?? "tr";
 
-            if(app != null)
+            var theme = JObject.Parse(app.Setting.AutTheme);
+
+            var application = new Application
             {
-                var theme = JObject.Parse(app.Setting.AutTheme);
-                var application = new Application()
+                Id = app.Id,
+                Name = app.Name,
+                Title = theme["title"].ToString(),
+                MultiLanguage = string.IsNullOrEmpty(app.Setting.Language),
+                Logo = app.Logo,
+                Theme = theme,
+                Color = theme["color"].ToString(),
+                CustomDomain = false,
+                Language = language,
+                Favicon = theme["favicon"].ToString(),
+                CdnUrl = cdnUrlStatic,
+                Domain = app.Setting.AppDomain,
+                Settings = new Settings
                 {
-                    Id = app.Id,
-                    Name = app.Name,
-                    Title = theme["title"].ToString(),
-                    MultiLanguage = string.IsNullOrEmpty(app.Setting.Language),
-                    Logo = app.Logo,
-                    Theme = theme,
-                    Color = theme["color"].ToString(),
-                    CustomDomain = false,
-                    Language = _language,
-                    Favicon = theme["favicon"].ToString(),
-                    CdnUrl = cdnUrlStatic,
-                    Domain = app.Setting.AppDomain,
-                    Settings = new Settings
-                    {
-                        Culture = app.Setting.Culture,
-                        Currency = app.Setting.Currency,
-                        TimeZone = app.Setting.TimeZone,
-                        GoogleAnalytics = app.Setting.GoogleAnalyticsCode,
-                        TenantOperationWebhook = app.Setting.TenantOperationWebhook,
-                    }
-                };
+                    Culture = app.Setting.Culture,
+                    Currency = app.Setting.Currency,
+                    TimeZone = app.Setting.TimeZone,
+                    GoogleAnalytics = app.Setting.GoogleAnalyticsCode,
+                    TenantOperationWebhook = app.Setting.TenantOperationWebhook,
+                }
+            };
 
-                return application;
-            }
-
-            return null;
+            return application;
         }
+
         public static string CurrentLanguage(HttpRequest request)
         {
             return !string.IsNullOrEmpty(request.Cookies[".AspNetCore.Culture"]) ? request.Cookies[".AspNetCore.Culture"].Split("uic=")[1] : null;
@@ -83,19 +77,18 @@ namespace PrimeApps.Auth.UI
 
         public static string GetClientId(string url)
         {
-            if (!string.IsNullOrEmpty(url))
-            {
-                var queryStrings = url.Split("&");
-                if (queryStrings.Length > 0)
-                {
-                    var clientIdIndex = Array.FindIndex(queryStrings, x => x.Contains("client_id"));
-                    if (clientIdIndex >= 0)
-                    {
-                        return queryStrings[clientIdIndex].Split("client_id=")[1];
-                    }
-                }
-            }
-            return null;
+            if (string.IsNullOrWhiteSpace(url))
+                throw new Exception("Url cannot be null.");
+
+            var returnUrl = HttpUtility.UrlDecode(url);
+
+            if (!returnUrl.StartsWith("http://") || !returnUrl.StartsWith("https://"))
+                returnUrl = "http://url.com" + returnUrl;
+
+            var uri = new Uri(returnUrl);
+            var clientId = HttpUtility.ParseQueryString(uri.Query).Get("client_id");
+
+            return clientId;
         }
 
         public static async Task<bool> TenantOperationWebhook(Application app, Tenant tenant, TenantUser tenantUser)
@@ -103,30 +96,30 @@ namespace PrimeApps.Auth.UI
             if (string.IsNullOrWhiteSpace(app.Settings.TenantOperationWebhook))
                 return true;
 
-			using (var httpClient = new HttpClient())
-			{
-				if (tenant.Id > 0)
-				{
-					var request = new JObject();
-					request["tenant_id"] = tenant.Id;
-					request["first_name"] = tenantUser.FirstName;
-					request["last_name"] = tenantUser.LastName;
-					request["phone"] = tenantUser.Phone;
-					request["email"] = tenantUser.Email;
+            using (var httpClient = new HttpClient())
+            {
+                if (tenant.Id > 0)
+                {
+                    var request = new JObject();
+                    request["tenant_id"] = tenant.Id;
+                    request["first_name"] = tenantUser.FirstName;
+                    request["last_name"] = tenantUser.LastName;
+                    request["phone"] = tenantUser.Phone;
+                    request["email"] = tenantUser.Email;
 
-					httpClient.DefaultRequestHeaders.Accept.Clear();
-					httpClient.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
-					var response = await httpClient.PostAsync(app.Settings.TenantOperationWebhook, new StringContent(JsonConvert.SerializeObject(request), Encoding.UTF8, "application/json"));
+                    httpClient.DefaultRequestHeaders.Accept.Clear();
+                    httpClient.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+                    var response = await httpClient.PostAsync(app.Settings.TenantOperationWebhook, new StringContent(JsonConvert.SerializeObject(request), Encoding.UTF8, "application/json"));
 
-					if (!response.IsSuccessStatusCode)
-					{
-						var resp = await response.Content.ReadAsStringAsync();
-						ErrorHandler.LogError(new Exception(resp),"Status Code: "+response.StatusCode +" tenant_id: " + tenant.Id + " first_name: " + tenantUser.FirstName + " last_name: " + tenantUser.LastName + " phone: " + tenantUser.Phone + " email: " + tenantUser.Email);
-					}
-				}
-			}
+                    if (!response.IsSuccessStatusCode)
+                    {
+                        var resp = await response.Content.ReadAsStringAsync();
+                        ErrorHandler.LogError(new Exception(resp), "Status Code: " + response.StatusCode + " tenant_id: " + tenant.Id + " first_name: " + tenantUser.FirstName + " last_name: " + tenantUser.LastName + " phone: " + tenantUser.Phone + " email: " + tenantUser.Email);
+                    }
+                }
+            }
 
-			return true;
-		}
-	}
+            return true;
+        }
+    }
 }
