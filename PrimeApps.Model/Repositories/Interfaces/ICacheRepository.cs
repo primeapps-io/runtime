c4f90d43fd2940f85b21087768fca7e902ca48ba
Galip Çevrik <galip.cevrik@ofisim.com>
2018-11-23 09:26:05 +0300

    Cache repository added

---
+++ PrimeApps.Model/Repositories/Interfaces/ICacheRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+
+namespace PrimeApps.Model.Repositories
+{
+    interface ICacheRepository
+    {
+        Task<T> Get<T>(string key);
+        Task<bool> Remove(string key);
+        Task<bool> Set(string key, object data);
+    }
+}
