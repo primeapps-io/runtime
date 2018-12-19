783522e88e26dee132898caf3fd324a93ab9dcec
Taner Özel <taner.ozel@ofisim.com>
2018-12-19 17:16:52 +0300

    #2894 Console css'lerin tekrar düzenlenmesi

--- PrimeApps.Console/wwwroot/view/organization/appform/appFormController.js
+++ PrimeApps.Console/wwwroot/view/organization/appform/appFormController.js
@@ -10,12 +10,13 @@ angular.module('primeapps')
             });
 
             uploader.filters.push({
-                name: 'syncFilter',
-                fn: function (item /*{File|FileLikeObject}*/, options) {
-                    console.log('syncFilter');
-                    return this.queue.length < 10;
+                name: 'imageFilter',
+                fn: function(item /*{File|FileLikeObject}*/, options) {
+                    var type = '|' + item.type.slice(item.type.lastIndexOf('/') + 1) + '|';
+                    return '|jpg|png|jpeg|bmp|gif|'.indexOf(type) !== -1;
                 }
             });
+
 
             // an async filter
             uploader.filters.push({
