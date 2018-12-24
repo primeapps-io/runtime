b5b4ae4a30f1257f3bdfc0809c78c0fd4fc7d82d
Taner Özel <taner.ozel@ofisim.com>
2018-12-24 17:39:05 +0300

    #2894 Console css'lerin tekrar düzenlenmesi

---
+++ PrimeApps.Console/wwwroot/view/app/templates/exceltemplates/excelTemplatesController.js
@@ -0,0 +1,12 @@
+﻿'use strict';
+
+angular.module('primeapps')
+
+    .controller('ExcelTemplatesController', ['$rootScope', '$scope', '$state', '$stateParams', '$location', 'ngToast', '$filter', '$cache', '$q', 'helper', 'dragularService', 'operators', 'ExcelTemplatesService',  '$http', 'config',
+        function ($rootScope, $scope, $state, $stateParams, $location, ngToast, $filter, $cache, $q, helper, dragularService, operators, ExcelTemplatesService, $http, config) {
+
+            $scope.$parent.menuTopTitle = "Templates";
+            $scope.$parent.activeMenu = 'templates';
+            $scope.$parent.activeMenuItem = 'templatesExcel';
+        }
+    ]);
