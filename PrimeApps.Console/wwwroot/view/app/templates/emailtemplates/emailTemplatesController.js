953e8a2782df1c1c707cc1e9ea2da9db3c5ed26f
Onur Bostancıoğlu <onur.bostancioglu@ofisim.com>
2018-12-26 13:43:08 +0300

    #2898 Templates

--- PrimeApps.Console/wwwroot/view/app/templates/emailtemplates/emailTemplatesController.js
+++ PrimeApps.Console/wwwroot/view/app/templates/emailtemplates/emailTemplatesController.js
@@ -2,11 +2,29 @@
 
 angular.module('primeapps')
 
-    .controller('EmailTemplatesController', ['$rootScope', '$scope', '$state', '$stateParams', '$location', 'ngToast', '$filter', '$cache', '$q', 'helper', 'dragularService', 'operators', 'EmailTemplatesService',  '$http', 'config',
-        function ($rootScope, $scope, $state, $stateParams, $location, ngToast, $filter, $cache, $q, helper, dragularService, operators, EmailTemplatesService, $http, config) {
+    .controller('EmailTemplatesController', ['$rootScope', '$scope', '$state', '$stateParams', '$location', 'ngToast', '$filter', '$cache', '$q', 'helper', 'dragularService', 'operators', 'EmailTemplatesService',  '$http', 'config','$modal',
+        function ($rootScope, $scope, $state, $stateParams, $location, ngToast, $filter, $cache, $q, helper, dragularService, operators, EmailTemplatesService, $http, config,$modal) {
 
             $scope.$parent.menuTopTitle = "Templates";
             $scope.$parent.activeMenu = 'templates';
             $scope.$parent.activeMenuItem = 'templatesEmail';
+
+            $scope.showFormModal = function () {
+
+                $scope.addNewEmailTemplateFormModal = $scope.addNewEmailTemplateFormModal || $modal({
+                    scope: $scope,
+                    templateUrl: 'view/app/templates/emailtemplates/emailTemplatesForm.html',
+                    animation: 'am-fade-and-slide-right',
+                    backdrop: 'static',
+                    show: false
+                });
+
+                $scope.addNewEmailTemplateFormModal.$promise.then(function () {
+                    // if (!relation.isNew)
+                    //     $scope.bindDragDrop();
+
+                    $scope.addNewEmailTemplateFormModal.show();
+                });
+            };
         }
     ]);
