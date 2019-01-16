41457a4381b0041d5903226b1c68b42bcf0dc9f7
Atakan Gemici <atakan.gemici@ofisim.com>
2019-01-16 22:29:57 +0300

    #2905 Authorization - Profiles

--- PrimeApps.Console/wwwroot/view/app/authorization/profiles/profilesService.js
+++ PrimeApps.Console/wwwroot/view/app/authorization/profiles/profilesService.js
@@ -5,8 +5,101 @@ angular.module('primeapps')
     .factory('ProfilesService', ['$rootScope', '$http', 'config', '$filter', '$q', 'helper',   '$cache', 'dataTypes', 'systemFields',
         function ($rootScope, $http, config, $filter, $q, helper,  $cache, dataTypes, systemFields) {
             return {
-                getReports: function () {
-                        console.log("Merhaba");
+                getAll: function () {
+                    return $http.post(config.apiUrl + 'Profile/GetAll', {});
+                },
+
+                getAllBasic: function () {
+                    return $http.get(config.apiUrl + 'Profile/GetAllBasic');
+                },
+
+                getProfiles: function (allProfiles, modules, clearPermissions) {
+                    var profiles = allProfiles;
+
+                    angular.forEach(profiles, function (profile) {
+                        if (profile.is_persistent && profile.has_admin_rights) {
+                            profile.name = $filter('translate')('Setup.Profiles.Administrator');
+                            profile.description = $filter('translate')('Setup.Profiles.AdministratorDescription');
+                        }
+
+                        if (profile.is_persistent && !profile.has_admin_rights) {
+                            profile.name = $filter('translate')('Setup.Profiles.Standard');
+                            profile.description = $filter('translate')('Setup.Profiles.StandardDescription');
+                        }
+
+                        if (clearPermissions) {
+                            if (profile.permissions) {
+                                profile.permissions = [];
+                            }
+                        }
+                        else {
+                            var permissions = [];
+
+                            angular.forEach(profile.permissions, function (permission) {
+                                var addPermission = true;
+
+                                switch (permission.type) {
+                                    case 1:/// Document
+                                        permission.EntityTypeName = $filter('translate')('Layout.Menu.Documents');
+                                        permission.order = 999;
+                                        break;
+                                    case 2:
+                                        permission.EntityTypeName = $filter('translate')('Layout.Menu.Reports');
+                                        permission.order = 1000;
+                                        break;
+                                    case 3:
+                                        permission.EntityTypeName = $filter('translate')('Feed.Feed');
+                                        permission.Order = 1001;
+                                        break;
+
+                                    case 0:/// Module
+                                        var module = $filter('filter')(modules, { id: permission.module_id }, true)[0];
+
+                                        if (module && module.order > 0) {
+                                            permission.EntityTypeName = module["label_" + $rootScope.language + "_plural"];
+                                            permission.order = module.order;
+                                        }
+                                        else {
+                                            addPermission = false;
+                                        }
+                                        break;
+                                }
+
+                                if (addPermission)
+                                    permissions.push(permission);
+                            });
+
+                            profile.permissions = $filter('orderBy')(permissions, 'Order');
+                        }
+                    });
+
+                    profiles = $filter('orderBy')(profiles, ['-is_persistent', '-has_admin_rights', '+name']);
+
+                    return profiles;
+                },
+
+                changeUserProfile: function (userId, tenantId, transferedProfileId) {
+                    return $http.post(config.apiUrl + 'Profile/ChangeUserProfile', {
+                        User_ID: userId,
+                        tenant_id: tenantId,
+                        Transfered_Profile_ID: transferedProfileId
+                    });
+                },
+
+                create: function (profile) {
+                    return $http.post(config.apiUrl + 'Profile/Create', profile);
+                },
+
+                update: function (profile) {
+                    return $http.post(config.apiUrl + 'Profile/Update', profile);
+                },
+
+                remove: function (removedProfileId, transferProfileId, tenantId) {
+                    return $http.post(config.apiUrl + 'Profile/Remove',
+                        {
+                            removed_profile: { id: removedProfileId, InstanceID: tenantId },
+                            transfer_profile: { id: transferProfileId, InstanceID: tenantId }
+                        });
                 }
             };
         }]);
