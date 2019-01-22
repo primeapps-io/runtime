72f37fbbf2bd1bdfc6cf652e780d7ccb2efde4a9
Onur Bostancıoğlu <onur.bostancioglu@ofisim.com>
2019-01-22 14:09:19 +0300

    #2907 Account Settings

---
+++ PrimeApps.Console/wwwroot/view/settings/password/passwordController.js
@@ -0,0 +1,37 @@
+'use strict';
+
+angular.module('primeapps')
+
+    .controller('PasswordController', ['$rootScope', '$scope', '$filter', '$modal', 'SettingService',
+        function ($rootScope, $scope, $filter, $modal, SettingService) {
+
+            $scope.$parent.menuTopTitle = "Settings";
+            $scope.$parent.activeMenuItem = 'password';
+
+            $scope.changePassword = function (passwordModel) {
+                if ($scope.passwordForm.$valid) {
+                    $scope.passwordUpdating = true;
+
+                    SettingService.changePassword(passwordModel.current, passwordModel.password, passwordModel.confirm)
+                        .then(function () {
+
+                            passwordModel.current = null;
+                            passwordModel.password = null;
+                            passwordModel.confirm = null;
+                            $scope.passwordForm.current.$setPristine();
+                            $scope.passwordForm.password.$setPristine();
+                            $scope.passwordForm.confirm.$setPristine();
+                            $scope.passwordForm.$setPristine();
+                            $scope.passwordUpdating = false;
+                            swal($filter('translate')('Setup.Settings.PasswordSuccess'), "", "success");
+                        })
+                        .catch(function (response) {
+                            if (response.status === 400)
+                                $scope.passwordForm.current.$setValidity('wrongPassword', false);
+
+                            $scope.passwordUpdating = false;
+                        });
+                }
+            };
+        }
+    ]);
