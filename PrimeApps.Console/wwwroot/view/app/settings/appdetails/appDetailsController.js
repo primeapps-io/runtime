b08d3cb8db28699a1f7274ae264871de6b59b9bb
Atakan Gemici <atakan.gemici@ofisim.com>
2019-01-22 17:31:32 +0300

    App Detail

--- PrimeApps.Console/wwwroot/view/app/settings/appdetails/appDetailsController.js
+++ PrimeApps.Console/wwwroot/view/app/settings/appdetails/appDetailsController.js
@@ -12,48 +12,48 @@ angular.module('primeapps')
             $scope.$parent.activeMenuItem = 'appDetails';
             $rootScope.breadcrumblist[2].title = 'App Details';
 
-            $scope.checkNameBlur = function () {
-                $scope.nameBlur = true;
-                $scope.checkName($scope.appModel.name);
-            };
-
-            $scope.checkName = function (name) {
-                if (!name)
-                    return;
-
-                $scope.appModel.name = name.replace(/\s/g, '');
-                $scope.appModel.name = name.replace(/[^a-zA-Z0-9\_\-]/g, '');
-
-                $scope.appModel.name = name.replace(/\s/g, '');
-                $scope.appModel.name = name.replace(/[^a-zA-Z0-9\_\-]/g, '');
-
-                if (!$scope.nameBlur)
-                    return;
-
-                $scope.nameChecking = true;
-                $scope.nameValid = null;
-
-                if (!name || name === '') {
-                    $scope.nameChecking = false;
-                    $scope.nameValid = false;
-                    return;
-                }
-
-                AppDetailsService.isUniqueName(name)
-                    .then(function (response) {
-                        $scope.nameChecking = false;
-                        if (response.data) {
-                            $scope.nameValid = true;
-                        }
-                        else {
-                            $scope.nameValid = false;
-                        }
-                    })
-                    .catch(function () {
-                        $scope.nameValid = false;
-                        $scope.nameChecking = false;
-                    });
-            };
+            // $scope.checkNameBlur = function () {
+            //     $scope.nameBlur = true;
+            //     $scope.checkName($scope.appModel.name);
+            // };
+            //
+            // $scope.checkName = function (name) {
+            //     if (!name)
+            //         return;
+            //
+            //     $scope.appModel.name = name.replace(/\s/g, '');
+            //     $scope.appModel.name = name.replace(/[^a-zA-Z0-9\_\-]/g, '');
+            //
+            //     $scope.appModel.name = name.replace(/\s/g, '');
+            //     $scope.appModel.name = name.replace(/[^a-zA-Z0-9\_\-]/g, '');
+            //
+            //     if (!$scope.nameBlur)
+            //         return;
+            //
+            //     $scope.nameChecking = true;
+            //     $scope.nameValid = null;
+            //
+            //     if (!name || name === '') {
+            //         $scope.nameChecking = false;
+            //         $scope.nameValid = false;
+            //         return;
+            //     }
+            //
+            //     AppDetailsService.isUniqueName(name)
+            //         .then(function (response) {
+            //             $scope.nameChecking = false;
+            //             if (response.data) {
+            //                 $scope.nameValid = true;
+            //             }
+            //             else {
+            //                 $scope.nameValid = false;
+            //             }
+            //         })
+            //         .catch(function () {
+            //             $scope.nameValid = false;
+            //             $scope.nameChecking = false;
+            //         });
+            // };
 
             AppDetailsService.get($scope.appId).then(function (response) {
                 var app = response.data;
