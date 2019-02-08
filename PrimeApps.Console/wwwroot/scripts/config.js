935c0ac8c2f34e9ceda53ba20a92bf728165d5c5
Fatih Sever <fatih.sever@ofisim.com>
2019-02-08 07:58:08 +0300

    (Rollback) Geçici olarak değiştirildi

--- PrimeApps.Console/wwwroot/scripts/config.js
+++ PrimeApps.Console/wwwroot/scripts/config.js
@@ -4,7 +4,7 @@ angular.module('primeapps')
 
     .constant('config', {
         apiUrl: 'api/',
-        appUrl: 'https://preview.primeapps.io',
+        appUrl: 'https://localhost:5001',
         clientId: '00000000000000000000000000000000',
         planIdMembers: '7673E999-18FB-497F-A958-84DCA43031CC',
         planIdUsers: 'F89E4FBF-A50F-40BA-BBEC-FE027F3F1524'
