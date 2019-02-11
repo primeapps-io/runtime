59d49aea9da3d37af217019cba9f6ca499fe5061
Bahadır Civelek <bahadir.civelek@ofisim.com>
2019-02-11 11:59:43 +0300

    deployments enums

---
+++ PrimeApps.Model/Enums/DeploymentStatus.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace PrimeApps.Model.Enums
+{
+    public enum DeploymentStatus
+    {
+        [EnumMember(Value = "")]
+        NotSet = 0,
+
+        [EnumMember(Value = "running")]
+        Running = 1,
+
+        [EnumMember(Value = "failed")]
+        Failed = 2,
+
+        [EnumMember(Value = "succeed")]
+        Succeed = 3
+    }
+}
