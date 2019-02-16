0bdde951c11d48b481eafde5343e955c1b579d9e
Fatih Sever <fatih.sever@ofisim.com>
2019-02-16 19:18:49 +0300

    #2886 Added again

--- PrimeApps.Model/Migrations/TenantDB/20190216161005_Task2886.Designer.cs
+++ PrimeApps.Model/Migrations/TenantDB/20190216161005_Task2886.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using PrimeApps.Model.Context;
@@ -9,9 +10,10 @@ using PrimeApps.Model.Context;
 namespace PrimeApps.Model.Migrations.TenantDB
 {
     [DbContext(typeof(TenantDBContext))]
-    partial class TenantDBContextModelSnapshot : ModelSnapshot
+    [Migration("20190216161005_Task2886")]
+    partial class Task2886
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
