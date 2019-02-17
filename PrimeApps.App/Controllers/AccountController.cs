ef2838181493bd7aee1915c1bf65be43e8e82a7b
Fatih Sever <fatih.sever@ofisim.com>
2019-02-17 11:25:32 +0300

    ChangePassword tekrar eklendi

--- PrimeApps.App/Controllers/AccountController.cs
+++ PrimeApps.App/Controllers/AccountController.cs
@@ -1,6 +1,11 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using PrimeApps.App.Models;
 using PrimeApps.Model.Repositories.Interfaces;
 
 namespace PrimeApps.App.Controllers
@@ -15,6 +20,32 @@ namespace PrimeApps.App.Controllers
             _applicationRepository = applicationRepository;
         }
 
+        [Route("change_password")]
+        public async Task<IActionResult> ChangePassword([FromBody]ChangePasswordBindingModel changePasswordBindingModel)
+        {
+            if (HttpContext.User.FindFirst("email") == null || string.IsNullOrEmpty(HttpContext.User.FindFirst("email").Value))
+                return Unauthorized();
+
+            changePasswordBindingModel.Email = HttpContext.User.FindFirst("email").Value;
+
+            var appInfo = await _applicationRepository.Get(Request.Host.Value);
+            using (var httpClient = new HttpClient())
+            {
+                var url = Request.Scheme + "://" + appInfo.Setting.AuthDomain + "/user/change_password?client=" + appInfo.Name;
+                httpClient.BaseAddress = new Uri(url);
+                httpClient.DefaultRequestHeaders.Accept.Clear();
+                httpClient.DefaultRequestHeaders.Add("Accept", "application/json");
+
+                var json = JsonConvert.SerializeObject(changePasswordBindingModel);
+                var response = await httpClient.PostAsync(url, new StringContent(json, Encoding.UTF8, "application/json"));
+
+                if (!response.IsSuccessStatusCode)
+                    return BadRequest(response);
+            }
+
+            return Ok();
+        }
+
         [Route("logout")]
         public async Task<IActionResult> Logout()
         {
@@ -23,7 +54,7 @@ namespace PrimeApps.App.Controllers
             Response.Cookies.Delete("tenant_id");
             await HttpContext.SignOutAsync();
 
-            return StatusCode(200, new {redirectUrl = Request.Scheme + "://" + appInfo.Setting.AuthDomain + "/Account/Logout?returnUrl=" + Request.Scheme + "://" + appInfo.Setting.AppDomain});
+            return StatusCode(200, new { redirectUrl = Request.Scheme + "://" + appInfo.Setting.AuthDomain + "/Account/Logout?returnUrl=" + Request.Scheme + "://" + appInfo.Setting.AppDomain });
         }
     }
 }
