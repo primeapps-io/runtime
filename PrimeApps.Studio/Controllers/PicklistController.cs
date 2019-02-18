98857f7e27a4bd62b6942a207958a97ce1f6e89e
Galip Çevrik <galip.cevrik@ofisim.com>
2019-02-18 17:51:22 +0300

    Picklists page ui service and controller added

--- PrimeApps.Studio/Controllers/PicklistController.cs
+++ PrimeApps.Studio/Controllers/PicklistController.cs
@@ -5,106 +5,126 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http.Extensions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
+using PrimeApps.Model.Common;
 using PrimeApps.Model.Repositories.Interfaces;
 using PrimeApps.Studio.Helpers;
 using PrimeApps.Studio.Models;
 using HttpStatusCode = Microsoft.AspNetCore.Http.StatusCodes;
 namespace PrimeApps.Studio.Controllers
 {
-	[Route("api/picklist"), Authorize]
-	public class PicklistController : DraftBaseController
-	{
-		private IPicklistRepository _picklistRepository;
+    [Route("api/picklist"), Authorize]
+    public class PicklistController : DraftBaseController
+    {
+        private IPicklistRepository _picklistRepository;
 
-		public PicklistController(IPicklistRepository picklistRepository)
-		{
-			_picklistRepository = picklistRepository;
-		}
+        public PicklistController(IPicklistRepository picklistRepository)
+        {
+            _picklistRepository = picklistRepository;
+        }
 
-		public override void OnActionExecuting(ActionExecutingContext context)
-		{
-			SetContext(context);
-			SetCurrentUser(_picklistRepository, PreviewMode, AppId, TenantId);
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            SetContext(context);
+            SetCurrentUser(_picklistRepository, PreviewMode, AppId, TenantId);
 
-			base.OnActionExecuting(context);
-		}
+            base.OnActionExecuting(context);
+        }
 
-		[Route("get/{id:int}"), HttpGet]
-		public async Task<IActionResult> Get(int id)
-		{
-			var picklistEntity = await _picklistRepository.GetById(id);
+        [Route("get/{id:int}"), HttpGet]
+        public async Task<IActionResult> Get(int id)
+        {
+            var picklistEntity = await _picklistRepository.GetById(id);
 
-			if (picklistEntity == null)
-				return NotFound();
+            if (picklistEntity == null)
+                return NotFound();
 
-			var picklistViewModel = PicklistHelper.MapToViewModel(picklistEntity);
+            var picklistViewModel = PicklistHelper.MapToViewModel(picklistEntity);
 
-			return Ok(picklistViewModel);
-		}
+            return Ok(picklistViewModel);
+        }
 
-		[Route("get_all"), HttpGet]
-		public async Task<IActionResult> GetAll()
-		{
-			var picklistEntities = await _picklistRepository.GetAll();
+        [Route("get_all"), HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var picklistEntities = await _picklistRepository.GetAll();
 
-			return Ok(picklistEntities);
-		}
+            return Ok(picklistEntities);
+        }
 
-		[Route("find"), HttpPost]
-		public async Task<IActionResult> Find([FromBody]List<int> ids)
-		{
-			var picklistEntities = await _picklistRepository.Find(ids);
-			var picklistsViewModel = PicklistHelper.MapToViewModel(picklistEntities);
+        //[Route("find"), HttpPost]
+        //public async Task<IActionResult> Find([FromBody]List<int> ids)
+        //{
+        //	var picklistEntities = await _picklistRepository.Find(ids);
+        //	var picklistsViewModel = PicklistHelper.MapToViewModel(picklistEntities);
 
-			return Ok(picklistsViewModel);
-		}
+        //	return Ok(picklistsViewModel);
+        //}
 
-		[Route("create"), HttpPost]
-		public async Task<IActionResult> Create([FromBody]PicklistBindingModel picklist)
-		{
-			if (!ModelState.IsValid)
-				return BadRequest(ModelState);
+        [Route("find"), HttpPost]
+        public async Task<IActionResult> Find([FromBody]PaginationModel paginationModel)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
 
-			var picklistEntity = PicklistHelper.CreateEntity(picklist);
-			var result = await _picklistRepository.Create(picklistEntity);
+            var picklists = await _picklistRepository.Find(paginationModel);
 
-			if (result < 1)
-				throw new ApplicationException(HttpStatusCode.Status500InternalServerError.ToString());
-			//throw new HttpResponseException(HttpStatusCode.Status500InternalServerError);
+            return Ok(picklists);
+        }
 
-			var uri = new Uri(Request.GetDisplayUrl());
-			return Created(uri.Scheme + "://" + uri.Authority + "/api/picklist/get/" + picklistEntity.Id, picklistEntity);
-			//return Created(Request.Scheme + "://" + Request.Host + "/api/picklist/get/" + picklistEntity.Id, picklistEntity);
-		}
+        [Route("count"), HttpGet]
+        public async Task<int> Count()
+        {
+            var count = await _picklistRepository.Count();
 
-		[Route("update/{id:int}"), HttpPut]
-		public async Task<IActionResult> Update(int id, [FromBody]PicklistBindingModel picklist)
-		{
-			if (!ModelState.IsValid)
-				return BadRequest(ModelState);
+            return count;
+        }
 
-			var picklistEntity = await _picklistRepository.GetById(id);
+        [Route("create"), HttpPost]
+        public async Task<IActionResult> Create([FromBody]PicklistBindingModel picklist)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
 
-			if (picklistEntity == null)
-				return NotFound();
+            var picklistEntity = PicklistHelper.CreateEntity(picklist);
+            var result = await _picklistRepository.Create(picklistEntity);
 
-			PicklistHelper.UpdateEntity(picklist, picklistEntity);
-			await _picklistRepository.Update(picklistEntity);
+            if (result < 1)
+                throw new ApplicationException(HttpStatusCode.Status500InternalServerError.ToString());
+            //throw new HttpResponseException(HttpStatusCode.Status500InternalServerError);
 
-			return Ok(picklistEntity);
-		}
+            var uri = new Uri(Request.GetDisplayUrl());
+            return Created(uri.Scheme + "://" + uri.Authority + "/api/picklist/get/" + picklistEntity.Id, picklistEntity);
+            //return Created(Request.Scheme + "://" + Request.Host + "/api/picklist/get/" + picklistEntity.Id, picklistEntity);
+        }
 
-		[Route("delete/{id:int}"), HttpDelete]
-		public async Task<IActionResult> Delete(int id)
-		{
-			var picklistEntity = await _picklistRepository.GetById(id);
+        [Route("update/{id:int}"), HttpPut]
+        public async Task<IActionResult> Update(int id, [FromBody]PicklistBindingModel picklist)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
 
-			if (picklistEntity == null)
-				return NotFound();
+            var picklistEntity = await _picklistRepository.GetById(id);
 
-			await _picklistRepository.DeleteSoft(picklistEntity);
+            if (picklistEntity == null)
+                return NotFound();
 
-			return Ok();
-		}
-	}
+            PicklistHelper.UpdateEntity(picklist, picklistEntity);
+            await _picklistRepository.Update(picklistEntity);
+
+            return Ok(picklistEntity);
+        }
+
+        [Route("delete/{id:int}"), HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var picklistEntity = await _picklistRepository.GetById(id);
+
+            if (picklistEntity == null)
+                return NotFound();
+
+            await _picklistRepository.DeleteSoft(picklistEntity);
+
+            return Ok();
+        }
+    }
 }
--- PrimeApps.Studio/wwwroot/view/app/model/picklists/pickListsService.js
+++ PrimeApps.Studio/wwwroot/view/app/model/picklists/pickListsService.js
@@ -1,12 +1,33 @@
+'use strict';
+
 angular.module('primeapps')
 
     .factory('PickListsService', ['$rootScope', '$http', 'config',
         function ($rootScope, $http, config) {
-            return {
-
+            return { 
+                get: function (id) {
+                    return $http.get(config.apiUrl + "picklist/get/" + id);
+                },
                 getAll: function () {
-                    return $http.post(config.apiUrl + 'view/create', view);
-                }
-
+                    return $http.get(config.apiUrl + 'picklist/get_all');
+                },
+                find: function (model) {
+                    return $http.post(config.apiUrl + 'picklist/find', model);
+                },
+                getPage: function (model) {
+                    return $http.post(config.apiUrl + 'picklist/get_page', model);
+                },
+                count: function () {
+                    return $http.get(config.apiUrl + 'picklist/count');
+                },
+                create: function (model) {
+                    return $http.post(config.apiUrl + 'picklist/create', model);
+                },
+                update: function (model) {
+                    return $http.put(config.apiUrl + 'picklist/update/' + id, model);
+                },
+                delete: function (id) {
+                    return $http.delete(config.apiUrl + 'picklist/delete/' + id);
+                } 
             };
         }]);
