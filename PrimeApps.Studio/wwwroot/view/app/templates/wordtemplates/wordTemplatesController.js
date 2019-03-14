9551bf22524c46eff0ecd233f29f332499041149
Onur Bostancıoğlu <onur.bostancioglu@ofisim.com>
2019-03-14 11:57:22 +0300

    #2898 Templates (Word/Excel)

--- PrimeApps.Studio/wwwroot/view/app/templates/wordtemplates/wordTemplatesController.js
+++ PrimeApps.Studio/wwwroot/view/app/templates/wordtemplates/wordTemplatesController.js
@@ -218,7 +218,10 @@ angular.module('primeapps')
 
             $scope.save = function (uploadForm) {
 
-                if ((uploadForm.$invalid || $scope.fileUpload.queue.length < 1) && !$scope.template.id ) {
+                if ((uploadForm.$invalid || !$scope.template.content) && $scope.template.id) {
+                    $scope.requiredColor = 'background-color:rgba(206, 4, 4, 0.15) !important;';
+                    return;
+                } else if (uploadForm.$invalid || $scope.fileUpload.queue.length < 1) {
                     $scope.requiredColor = 'background-color:rgba(206, 4, 4, 0.15) !important;';
                     return;
                 }
