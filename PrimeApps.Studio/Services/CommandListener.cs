67f7e780084169991aad8f6ca8e7fa0a740cfd07
İbrahim Varol <ibrahim.varol@ofisim.com>
2019-04-18 23:55:25 +0300

    Query  functions (initial)

--- PrimeApps.Studio/Services/CommandListener.cs
+++ PrimeApps.Studio/Services/CommandListener.cs
@@ -1,9 +1,12 @@
 using System;
+using System.Data;
 using System.Data.Common;
 using System.Diagnostics;
 using System.Linq;
+using System.Text.RegularExpressions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.EntityFrameworkCore.Storage;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DiagnosticAdapter;
@@ -17,7 +20,6 @@ namespace PrimeApps.Studio.Services
     {
         private IBackgroundTaskQueue _queue;
         private IApplicationBuilder _app;
-
         public CommandListener(IBackgroundTaskQueue queue, IApplicationBuilder app)
         {
             _queue = queue;
@@ -27,40 +29,59 @@ namespace PrimeApps.Studio.Services
         [DiagnosticName("Microsoft.EntityFrameworkCore.Database.Command.CommandExecuting")]
         public void OnCommandExecuting(DbCommand command, DbCommandMethod executeMethod, Guid commandId, Guid connectionId, bool async, DateTimeOffset startTime)
         {
-            if (command.CommandText.ToUpper().StartsWith("INSERT") || command.CommandText.ToUpper().StartsWith("UPDATE") || command.CommandText.ToUpper().StartsWith("DELETE"))
-            {
-                _queue.QueueBackgroundWorkItem(token =>
-                {
-                    using (var scope = _app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
-                    {
-                        var _configuration = _app.ApplicationServices.GetService<IConfiguration>();
-
-                        var databaseContext = scope.ServiceProvider.GetRequiredService<StudioDBContext>();
-    
-                        using (var organizationRepository = new OrganizationRepository(databaseContext, _configuration))
-                        {
-                            var check = organizationRepository.IsOrganizationAvaliable(637, 276);
-                        }
-                    }
-
-                    return null;
-                });
-
-
-                Console.WriteLine("OnCommandExecuting");
-            }
+            Console.WriteLine("OnCommandExecuting");
         }
 
         [DiagnosticName("Microsoft.EntityFrameworkCore.Database.Command.CommandExecuted")]
-        public void OnCommandExecuted(object result, bool async)
+        public void OnCommandExecuted(RelationalDataReader readerResult, bool async)
         {
-            Console.WriteLine("OnCommandExecuted");
+            if (readerResult.DbCommand.CommandText.StartsWith("INSERT", true, null) ||
+             readerResult.DbCommand.CommandText.StartsWith("UPDATE", true, null) ||
+              readerResult.DbCommand.CommandText.StartsWith("DELETE", true, null))
+            {
+                Console.WriteLine("OnCommandExecuted");
+                string rawQuery = GetGeneratedQuery(readerResult.DbCommand);
+                string result = GetTableName(rawQuery);
+
+                // _queue.QueueBackgroundWorkItem(token =>
+                // {
+                //     using (var scope = _app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
+                //     {
+                //         var _configuration = _app.ApplicationServices.GetService<IConfiguration>();
+                //         var databaseContext = scope.ServiceProvider.GetRequiredService<StudioDBContext>();
+                //         using (var organizationRepository = new OrganizationRepository(databaseContext, _configuration))
+                //         {
+                //             var check = organizationRepository.IsOrganizationAvaliable(637, 276);
+                //         }
+                //     }
+                //     return null;
+                // });
+            }
         }
 
         [DiagnosticName("Microsoft.EntityFrameworkCore.Database.Command.CommandError")]
         public void OnCommandError(Exception exception, bool async)
         {
             Console.WriteLine("OnCommandError");
+        }
+
+        public string GetGeneratedQuery(DbCommand dbCommand)
+        {
+            var query = dbCommand.CommandText;
+            foreach (DbParameter parameter in dbCommand.Parameters)
+            {
+                query = query.Replace(parameter.ParameterName, parameter.Value.ToString());
+            }
+            return query;
+        }
+
+        public string GetTableName(string query)
+        {
+            Regex nameExtractor = new Regex("((?<=INSERT\\sINTO\\s)|(?<=UPDATE\\s)|(?<=DELETE\\sFROM\\s))([^\\s]+)");
+
+            Match match = nameExtractor.Match(query);
+
+            return match.Value;
         }
     }
 }
