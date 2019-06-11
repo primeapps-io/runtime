65ab24d728932d1d40612880596755f7677355a3
Onur Bostancıoğlu <onur.bostancioglu@ofisim.com>
2019-06-11 15:29:49 +0300

    AppModules bug fix

--- PrimeApps.Studio/wwwroot/view/app/model/modules/moduleController.js
+++ PrimeApps.Studio/wwwroot/view/app/model/modules/moduleController.js
@@ -64,7 +64,6 @@ angular.module('primeapps')
                     ModuleService.find(requestModel)
                         .then(function (response) {
                             $scope.modules = response.data;
-                            $rootScope.appModules = $scope.modules;
                             $scope.loading = false;
                         });
 
