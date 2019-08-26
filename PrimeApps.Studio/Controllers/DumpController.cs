defbcb31595da38ee394fe6eb1945a0ff96dbdcd
Taner Özel <taner.ozel@ofisim.com>
2019-08-26 14:19:58 +0300

    publish

--- PrimeApps.Studio/Controllers/DumpController.cs
+++ PrimeApps.Studio/Controllers/DumpController.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using Hangfire;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Hosting;
@@ -130,17 +131,25 @@ namespace PrimeApps.Studio.Controllers
             switch ((string)request["environment"])
             {
                 case "test":
-                    _posgresHelper.Drop("PlatformDBConnectionTest", app, dumpDirectory);
+                    try
+                    {
+                        _posgresHelper.Drop("PlatformDBConnectionTest", app, dumpDirectory);
+                    }
+                    catch (Exception e)
+                    {
+                       
+                    } 
+
                     _posgresHelper.Create("PlatformDBConnectionTest", app, dumpDirectory);
                     _posgresHelper.Restore("PlatformDBConnectionTest", app, dumpDirectory, app, dumpDirectory);
-                break;
+                    break;
                 case "prod":
                     _posgresHelper.Drop("PlatformDBConnection", app, dumpDirectory);
                     _posgresHelper.Create("PlatformDBConnection", app, dumpDirectory);
                     _posgresHelper.Restore("PlatformDBConnection", app, dumpDirectory, app, dumpDirectory);
                     break;
             }
-         
+
 
 
             return Ok();
