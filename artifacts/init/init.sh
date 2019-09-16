e1816e28df7971b5a664408ee414f5c8b965405e
İbrahim <ibrahim.varol@primeapps.io>
2019-09-16 02:43:29 +0200

    Init Container RC-3

--- artifacts/init/init.sh
+++ artifacts/init/init.sh
@@ -2,19 +2,23 @@
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 sleep 30
+
 echo -e "${GREEN}Creating Cluster...${NC}"
-pgo create cluster -w 'pr!m€Appsi0d' --metrics --pgbackrest --custom-config 'pgo-custom-pg-config' primeapps-database
-echo -e "${GREEN}Done.${NC}"
 
-until pg_isready; do echo waiting for cluster; sleep 15; done;
+echo "primeapps:$PGOPASSWORD:pgoadmin
+primeappstest:$PGOPASSWORD:pgoadmin
+primeappsreadonly:$PGOPASSWORD:pgoreader" > .pgouser
 
+pgo create cluster -w $PGPASSWORD --metrics --pgbackrest --custom-config 'pgo-custom-pg-config' primeapps-database
 
-echo -e "${GREEN}Creating Databases:${NC}"
+until pg_isready; do echo Waiting for cluster...; sleep 15; done;
+
+echo -e "${GREEN}Creating Databases...${NC}"
 createdb --template=template0 --encoding=UTF8 auth
 createdb --template=template0 --encoding=UTF8 platform
-echo -e "${GREEN}Done.${NC}"
 
-echo -e "${GREEN}Restoring Databases:${NC}"
-pg_restore -w -Fc --clean -e -d platform /root/platform.dmp
-pg_restore -w -Fc --clean -e -d auth /root/auth.dmp
-echo -e "${GREEN}Done.${NC}"
+echo -e "${GREEN}Restoring Databases...${NC}"
+pg_restore -Fc -d platform /root/platform.dmp
+pg_restore -Fc -d auth /root/auth.dmp
+
+echo -e "${GREEN}Done!${NC}"
