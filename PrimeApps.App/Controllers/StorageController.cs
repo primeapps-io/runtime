1007a19dfb1dcc56d5e59ba4143925dbdb3d61bf
Atakan Gemici <atakan.gemici@ofisim.com>
2019-11-18 14:58:15 +0300

    #3291 FW: Doküman Minio Olayı

--- PrimeApps.App/Controllers/StorageController.cs
+++ PrimeApps.App/Controllers/StorageController.cs
@@ -199,6 +199,7 @@ namespace PrimeApps.App.Controllers
                 var ext = Path.GetExtension(parser.Filename);
 
                 var fileName = Path.GetFileNameWithoutExtension(parser.Filename);
+                fileName = fileName.Replace(" ", "_");
 
                 var uniqueName = fileName + "_" + DateTime.UtcNow.ToFileTimeUtc() + ext;
 
