e9d8ed0db8aae428517a22123e95169bf8516510
Taner Özel <taner.ozel@ofisim.com>
2019-11-29 14:28:30 +0300

    #3446 Raporlara ve Dashboarda Çoklu Dil Özelliği Kazandırlması

---
+++ PrimeApps.Model/Migrations/TenantDB/20191128011240_Task3446-2.cs
@@ -0,0 +1,317 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PrimeApps.Model.Migrations.TenantDB
+{
+    public partial class Task34462 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "name_tr",
+                schema: "public",
+                table: "widgets",
+                maxLength: 200,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 200);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "name_en",
+                schema: "public",
+                table: "widgets",
+                maxLength: 200,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 200);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "name_tr",
+                schema: "public",
+                table: "reports",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "name_en",
+                schema: "public",
+                table: "reports",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "name_tr",
+                schema: "public",
+                table: "report_categories",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "name_en",
+                schema: "public",
+                table: "report_categories",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "name_tr",
+                schema: "public",
+                table: "dashlets",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "name_en",
+                schema: "public",
+                table: "dashlets",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "name_tr",
+                schema: "public",
+                table: "dashboard",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "name_en",
+                schema: "public",
+                table: "dashboard",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "y_axis_name_tr",
+                schema: "public",
+                table: "charts",
+                maxLength: 80,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 80);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "y_axis_name_en",
+                schema: "public",
+                table: "charts",
+                maxLength: 80,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 80);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "x_axis_name_tr",
+                schema: "public",
+                table: "charts",
+                maxLength: 80,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 80);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "x_axis_name_en",
+                schema: "public",
+                table: "charts",
+                maxLength: 80,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 80);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "caption_tr",
+                schema: "public",
+                table: "charts",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "caption_en",
+                schema: "public",
+                table: "charts",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 100);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "name_tr",
+                schema: "public",
+                table: "widgets",
+                maxLength: 200,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 200,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "name_en",
+                schema: "public",
+                table: "widgets",
+                maxLength: 200,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 200,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "name_tr",
+                schema: "public",
+                table: "reports",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 100,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "name_en",
+                schema: "public",
+                table: "reports",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 100,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "name_tr",
+                schema: "public",
+                table: "report_categories",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 100,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "name_en",
+                schema: "public",
+                table: "report_categories",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 100,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "name_tr",
+                schema: "public",
+                table: "dashlets",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 50,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "name_en",
+                schema: "public",
+                table: "dashlets",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 50,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "name_tr",
+                schema: "public",
+                table: "dashboard",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 50,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "name_en",
+                schema: "public",
+                table: "dashboard",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 50,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "y_axis_name_tr",
+                schema: "public",
+                table: "charts",
+                maxLength: 80,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 80,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "y_axis_name_en",
+                schema: "public",
+                table: "charts",
+                maxLength: 80,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 80,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "x_axis_name_tr",
+                schema: "public",
+                table: "charts",
+                maxLength: 80,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 80,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "x_axis_name_en",
+                schema: "public",
+                table: "charts",
+                maxLength: 80,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 80,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "caption_tr",
+                schema: "public",
+                table: "charts",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 100,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "caption_en",
+                schema: "public",
+                table: "charts",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 100,
+                oldNullable: true);
+        }
+    }
+}
