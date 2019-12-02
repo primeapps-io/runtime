1a941de564908ea31c382bb2f8037d90199a6118
Fatih Sever <fatih.sever@ofisim.com>
2019-12-02 19:03:05 +0300

    Bugfix

--- PrimeApps.App/Controllers/TemplateController.cs
+++ PrimeApps.App/Controllers/TemplateController.cs
@@ -15,130 +15,136 @@ using HttpStatusCode = Microsoft.AspNetCore.Http.StatusCodes;
 
 namespace PrimeApps.App.Controllers
 {
-	[Route("api/template"), Authorize]
-	public class TemplateController : ApiBaseController
-	{
-		private readonly ITemplateRepository _templateRepostory;
-		private readonly IUserRepository _userRepository;
-		private readonly IRecordRepository _recordRepository;
-		private readonly IModuleRepository _moduleRepository;
-		private IConfiguration _configuration;
+    [Route("api/template"), Authorize]
+    public class TemplateController : ApiBaseController
+    {
+        private readonly ITemplateRepository _templateRepostory;
+        private readonly IUserRepository _userRepository;
+        private readonly IRecordRepository _recordRepository;
+        private readonly IModuleRepository _moduleRepository;
+        private IConfiguration _configuration;
 
-		public TemplateController(ITemplateRepository templateRepostory, IUserRepository userRepository, IRecordRepository recordRepository, IModuleRepository moduleRepository, IConfiguration configuration)
-		{
-			_templateRepostory = templateRepostory;
-			_userRepository = userRepository;
-			_recordRepository = recordRepository;
-			_moduleRepository = moduleRepository;
-			_configuration = configuration;
-		}
+        public TemplateController(ITemplateRepository templateRepostory, IUserRepository userRepository, IRecordRepository recordRepository, IModuleRepository moduleRepository, IConfiguration configuration)
+        {
+            _templateRepostory = templateRepostory;
+            _userRepository = userRepository;
+            _recordRepository = recordRepository;
+            _moduleRepository = moduleRepository;
+            _configuration = configuration;
+        }
 
-		public override void OnActionExecuting(ActionExecutingContext context)
-		{
-			SetContext(context);
-			SetCurrentUser(_userRepository, PreviewMode, TenantId, AppId);
-			SetCurrentUser(_templateRepostory, PreviewMode, TenantId, AppId);
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            SetContext(context);
+            SetCurrentUser(_userRepository, PreviewMode, TenantId, AppId);
+            SetCurrentUser(_templateRepostory, PreviewMode, TenantId, AppId);
 
-			base.OnActionExecuting(context);
-		}
+            base.OnActionExecuting(context);
+        }
 
-		[Route("get/{id:int}"), HttpGet]
-		public async Task<IActionResult> Get(int id)
-		{
-			var template = await _templateRepostory.GetById(id);
+        [Route("get/{id:int}"), HttpGet]
+        public async Task<IActionResult> Get(int id)
+        {
+            var template = await _templateRepostory.GetById(id);
 
-			if (template == null)
-				return NotFound();
+            if (template == null)
+                return NotFound();
 
-			return Ok(template);
-		}
+            return Ok(template);
+        }
 
-		[Route("get_all"), HttpGet]
-		public async Task<IActionResult> GetAll([FromQuery(Name = "type")]TemplateType type = TemplateType.NotSet, [FromQuery(Name = "moduleName")]string moduleName = "")
-		{
-			var language = AppUser.Language.ToEnum<LanguageType>();
-			var templates = await _templateRepostory.GetAll(type, language, true, moduleName);
+        [Route("get_all"), HttpGet]
+        public async Task<IActionResult> GetAll([FromQuery(Name = "type")]TemplateType type = TemplateType.NotSet, [FromQuery(Name = "moduleName")]string moduleName = "")
+        {
+            var language = AppUser.Language.ToEnum<LanguageType>();
+            var templates = await _templateRepostory.GetAll(type, language, true, moduleName);
 
-			return Ok(templates);
-		}
+            return Ok(templates);
+        }
 
-		[Route("get_all_list"), HttpGet]
-		public async Task<IActionResult> GetAllList(TemplateType type = TemplateType.NotSet, TemplateType excelType = TemplateType.NotSet, string moduleName = "")
-		{
-			var language = AppUser.Language.ToEnum<LanguageType>();
-			var templates = await _templateRepostory.GetAllList(language, type, excelType, moduleName);
+        [Route("get_all_list"), HttpGet]
+        public async Task<IActionResult> GetAllList(TemplateType type = TemplateType.NotSet, TemplateType excelType = TemplateType.NotSet, string moduleName = "")
+        {
+            var language = AppUser.Language.ToEnum<LanguageType>();
+            var templates = await _templateRepostory.GetAllList(language, type, excelType, moduleName);
 
-			return Ok(templates);
-		}
+            return Ok(templates);
+        }
 
-		[Route("create"), HttpPost]
-		public async Task<IActionResult> Create([FromBody]TemplateBindingModel template)
-		{
-			if (!ModelState.IsValid)
-				return BadRequest(ModelState);
+        [Route("create"), HttpPost]
+        public async Task<IActionResult> Create([FromBody]TemplateBindingModel template)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
 
-			var templateEntity = TemplateHelper.CreateEntity(template, _userRepository);
-			var result = await _templateRepostory.Create(templateEntity);
+            if (template.Language == LanguageType.NotSet)
+                template.Language = AppUser.Language.ToEnum<LanguageType>();
 
-			if (result < 1)
-				throw new ApplicationException(HttpStatusCode.Status500InternalServerError.ToString());
+            var templateEntity = TemplateHelper.CreateEntity(template, _userRepository);
+            var result = await _templateRepostory.Create(templateEntity);
 
-			if (template.Chunks > 0)
-				await AzureStorage.CommitFile(template.Content, $"templates/{template.Content}", template.ContentType, string.Format("inst-{0}", AppUser.TenantGuid), template.Chunks, _configuration);
+            if (result < 1)
+                throw new ApplicationException(HttpStatusCode.Status500InternalServerError.ToString());
 
-			var uri = new Uri(Request.GetDisplayUrl());
-			return Created(uri.Scheme + "://" + uri.Authority + "/api/template/get/" + templateEntity.Id, templateEntity);
-		}
+            if (template.Chunks > 0)
+                await AzureStorage.CommitFile(template.Content, $"templates/{template.Content}", template.ContentType, string.Format("inst-{0}", AppUser.TenantGuid), template.Chunks, _configuration);
 
-		[Route("create_excel"), HttpPost]
-		public async Task<IActionResult> CreateExcel(TemplateBindingModel template)
-		{
-			if (!ModelState.IsValid)
-				return BadRequest(ModelState);
+            var uri = new Uri(Request.GetDisplayUrl());
+            return Created(uri.Scheme + "://" + uri.Authority + "/api/template/get/" + templateEntity.Id, templateEntity);
+        }
 
-			var templateEntity = TemplateHelper.CreateEntityExcel(template, _userRepository);
-			var result = await _templateRepostory.Create(templateEntity);
+        [Route("create_excel"), HttpPost]
+        public async Task<IActionResult> CreateExcel(TemplateBindingModel template)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
 
-			if (result < 1)
-				throw new ApplicationException(HttpStatusCode.Status500InternalServerError.ToString());
+            var templateEntity = TemplateHelper.CreateEntityExcel(template, _userRepository);
+            var result = await _templateRepostory.Create(templateEntity);
 
-			if (template.Chunks > 0)
-				await AzureStorage.CommitFile(template.Content, $"templates/{template.Content}", template.ContentType, string.Format("inst-{0}", AppUser.TenantGuid), template.Chunks, _configuration);
+            if (result < 1)
+                throw new ApplicationException(HttpStatusCode.Status500InternalServerError.ToString());
 
-			return Created(Request.Scheme + "://" + Request.Host + "/api/template/get/" + templateEntity.Id, templateEntity);
-		}
+            if (template.Chunks > 0)
+                await AzureStorage.CommitFile(template.Content, $"templates/{template.Content}", template.ContentType, string.Format("inst-{0}", AppUser.TenantGuid), template.Chunks, _configuration);
 
-		[Route("update/{id:int}"), HttpPut]
-		public async Task<IActionResult> Update(int id, [FromBody]TemplateBindingModel template)
-		{
-			if (!ModelState.IsValid)
-				return BadRequest(ModelState);
+            return Created(Request.Scheme + "://" + Request.Host + "/api/template/get/" + templateEntity.Id, templateEntity);
+        }
 
-			var templateEntity = await _templateRepostory.GetById(id);
+        [Route("update/{id:int}"), HttpPut]
+        public async Task<IActionResult> Update(int id, [FromBody]TemplateBindingModel template)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
 
-			if (templateEntity == null)
-				return NotFound();
+            var templateEntity = await _templateRepostory.GetById(id);
 
-			TemplateHelper.UpdateEntity(template, templateEntity, _userRepository);
-			await _templateRepostory.Update(templateEntity);
+            if (templateEntity == null)
+                return NotFound();
 
-			if (template.Chunks > 0)
-				await AzureStorage.CommitFile(template.Content, $"templates/{template.Content}", template.ContentType, string.Format("inst-{0}", AppUser.TenantGuid), template.Chunks, _configuration);
+            if (template.Language == LanguageType.NotSet)
+                template.Language = AppUser.Language.ToEnum<LanguageType>();
 
-			return Ok(templateEntity);
-		}
+            TemplateHelper.UpdateEntity(template, templateEntity, _userRepository);
+            await _templateRepostory.Update(templateEntity);
 
-		[Route("delete/{id:int}"), HttpDelete]
-		public async Task<IActionResult> Delete(int id)
-		{
-			var templateEntity = await _templateRepostory.GetById(id);
+            if (template.Chunks > 0)
+                await AzureStorage.CommitFile(template.Content, $"templates/{template.Content}", template.ContentType, string.Format("inst-{0}", AppUser.TenantGuid), template.Chunks, _configuration);
 
-			if (templateEntity == null)
-				return NotFound();
+            return Ok(templateEntity);
+        }
 
-			await _templateRepostory.DeleteSoft(templateEntity);
+        [Route("delete/{id:int}"), HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var templateEntity = await _templateRepostory.GetById(id);
 
-			return Ok();
-		}
-	}
+            if (templateEntity == null)
+                return NotFound();
+
+            await _templateRepostory.DeleteSoft(templateEntity);
+
+            return Ok();
+        }
+    }
 }
