1baf3995bd52edab8830d36b21d0482f3808b6f5
bahadircivelek@gmail.com <bahadircivelek@gmail.com>
2019-12-24 18:48:15 +0300

    role base info issue

--- PrimeApps.App/Startup/DIConfig.cs
+++ PrimeApps.App/Startup/DIConfig.cs
@@ -43,7 +43,7 @@ namespace PrimeApps.App
                             throw new Exception("The " + itype.Name + " type has more than one implementations, please change your filter");
                         }
 
-                        services.AddTransient(itype, type);
+                        services.AddScoped(itype, type);
                     }
                 }
             }
