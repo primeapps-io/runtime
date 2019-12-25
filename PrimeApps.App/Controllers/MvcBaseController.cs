35d43005ac8e89a3b22575195a7bd8f2d8c6567c
bahadircivelek@gmail.com <bahadircivelek@gmail.com>
2019-12-25 19:30:56 +0300

    Role base info issue fixed.

--- PrimeApps.App/Controllers/MvcBaseController.cs
+++ PrimeApps.App/Controllers/MvcBaseController.cs
@@ -15,9 +15,9 @@ namespace PrimeApps.App.Controllers
     [Authorize, CheckHttpsRequire, ResponseCache(CacheProfileName = "Nocache")]
     public class MvcBaseController : BaseController
     {
-        public static int? AppId { get; set; }
-        public static int? TenantId { get; set; }
-        public static string PreviewMode { get; set; }
+        public int? AppId { get; set; }
+        public int? TenantId { get; set; }
+        public string PreviewMode { get; set; }
 
         public void SetContext(ActionExecutingContext context)
         {
