dc42df9b8117cca52200524a75d0219c093bf777
Onur Bostancıoğlu <onur.bostancioglu@primeapps.io>
2020-01-08 16:38:25 +0300

    #3795 SendExternalEmail method adının SendEmail olarak değiştirilmesi

---
+++ PrimeApps.Model/Common/Messaging/EmailJobRequest.cs
@@ -0,0 +1,49 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
+using PrimeApps.Model.Enums;
+
+namespace PrimeApps.Model.Common.Messaging
+{
+    [DataContract]
+    public class EmailJobRequest
+    {
+        [DataMember]
+        [Required]
+        public int ModuleId { get; set; }
+        [DataMember]
+        public string[] Ids { get; set; }
+        [DataMember]
+        public string Query { get; set; }
+
+        [DataMember]
+        public bool IsAllSelected { get; set; }
+        [DataMember]
+        [Required]
+        public string Subject { get; set; }
+        [DataMember]
+        [Required]
+        public int TemplateId { get; set; }
+        [DataMember]
+        [Required]
+        public string EMailField { get; set; }
+        [DataMember]
+        [Required]
+        public string SenderAlias { get; set; }
+        [DataMember]
+        [Required]
+        public AccessLevelEnum ProviderType { get; set; }
+        [DataMember]
+        [Required]
+        public string SenderEMail { get; set; }
+        [DataMember]
+        public string AttachmentContainer { get; set; }
+        [DataMember]
+        public string AttachmentLink { get; set; }
+        [DataMember]
+        public string AttachmentName { get; set; }
+        [DataMember]
+        public string Cc { get; set; }
+        [DataMember]
+        public string Bcc { get; set; }
+    }
+}
