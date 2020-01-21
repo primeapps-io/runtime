34d4bf8c64ccb36979e5f7931652ee9dd6b23279
Onur Bostancıoğlu <onur.bostancioglu@primeapps.io>
2020-01-21 13:56:00 +0300

    #3797 Paket oluştururken sadece seçilen modül verilerinin silinmesi

--- PrimeApps.Studio/Controllers/PackageController.cs
+++ PrimeApps.Studio/Controllers/PackageController.cs
@@ -27,241 +27,243 @@ using Microsoft.AspNet.OData.Extensions;
 
 namespace PrimeApps.Studio.Controllers
 {
-    [Route("api/package")]
-    public class PackageController : DraftBaseController
-    {
-        private IConfiguration _configuration;
-        private IBackgroundTaskQueue _queue;
-        private IPackageRepository _packageRepository;
-        private IAppDraftRepository _appDraftRepository;
-        private IHistoryDatabaseRepository _historyDatabaseRepository;
-        private IHistoryStorageRepository _historyStorageRepository;
-        private IPermissionHelper _permissionHelper;
-        private IPackageHelper _packageHelper;
-        private IUnifiedStorage _storage;
-        private IHostingEnvironment _hostingEnvironment;
+	[Route("api/package")]
+	public class PackageController : DraftBaseController
+	{
+		private IConfiguration _configuration;
+		private IBackgroundTaskQueue _queue;
+		private IPackageRepository _packageRepository;
+		private IAppDraftRepository _appDraftRepository;
+		private IHistoryDatabaseRepository _historyDatabaseRepository;
+		private IHistoryStorageRepository _historyStorageRepository;
+		private IPermissionHelper _permissionHelper;
+		private IPackageHelper _packageHelper;
+		private IUnifiedStorage _storage;
+		private IHostingEnvironment _hostingEnvironment;
 
-        public PackageController(IConfiguration configuration,
-            IPackageHelper packageHelper,
-            IPackageRepository packageRepository,
-            IAppDraftRepository appDraftRepository,
-            IHistoryDatabaseRepository historyDatabaseRepository,
-            IHistoryStorageRepository historyStorageRepository,
-            IPermissionHelper permissionHelper,
-            IUnifiedStorage storage,
-            IBackgroundTaskQueue queue,
-            IHostingEnvironment hostingEnvironment)
-        {
-            _configuration = configuration;
-            _packageHelper = packageHelper;
-            _packageRepository = packageRepository;
-            _historyDatabaseRepository = historyDatabaseRepository;
-            _historyStorageRepository = historyStorageRepository;
-            _appDraftRepository = appDraftRepository;
-            _permissionHelper = permissionHelper;
-            _storage = storage;
-            _queue = queue;
-            _hostingEnvironment = hostingEnvironment;
-        }
+		public PackageController(IConfiguration configuration,
+			IPackageHelper packageHelper,
+			IPackageRepository packageRepository,
+			IAppDraftRepository appDraftRepository,
+			IHistoryDatabaseRepository historyDatabaseRepository,
+			IHistoryStorageRepository historyStorageRepository,
+			IPermissionHelper permissionHelper,
+			IUnifiedStorage storage,
+			IBackgroundTaskQueue queue,
+			IHostingEnvironment hostingEnvironment)
+		{
+			_configuration = configuration;
+			_packageHelper = packageHelper;
+			_packageRepository = packageRepository;
+			_historyDatabaseRepository = historyDatabaseRepository;
+			_historyStorageRepository = historyStorageRepository;
+			_appDraftRepository = appDraftRepository;
+			_permissionHelper = permissionHelper;
+			_storage = storage;
+			_queue = queue;
+			_hostingEnvironment = hostingEnvironment;
+		}
 
-        public override void OnActionExecuting(ActionExecutingContext context)
-        {
-            SetContext(context);
-            SetCurrentUser(_packageRepository);
-            SetCurrentUser(_historyDatabaseRepository, PreviewMode, TenantId, AppId);
-            SetCurrentUser(_historyStorageRepository, PreviewMode, TenantId, AppId);
-            base.OnActionExecuting(context);
-        }
+		public override void OnActionExecuting(ActionExecutingContext context)
+		{
+			SetContext(context);
+			SetCurrentUser(_packageRepository);
+			SetCurrentUser(_historyDatabaseRepository, PreviewMode, TenantId, AppId);
+			SetCurrentUser(_historyStorageRepository, PreviewMode, TenantId, AppId);
+			base.OnActionExecuting(context);
+		}
 
-        [Route("log/{id}"), HttpGet]
-        public async Task<IActionResult> Log(int id)
-        {
-            if (UserProfile != ProfileEnum.Manager && !_permissionHelper.CheckUserProfile(UserProfile, "package", RequestTypeEnum.View))
-                return StatusCode(403);
+		[Route("log/{id}"), HttpGet]
+		public async Task<IActionResult> Log(int id)
+		{
+			if (UserProfile != ProfileEnum.Manager && !_permissionHelper.CheckUserProfile(UserProfile, "package", RequestTypeEnum.View))
+				return StatusCode(403);
 
-            var package = await _packageRepository.Get(id);
+			var package = await _packageRepository.Get(id);
 
-            if (package == null)
-                return BadRequest();
+			if (package == null)
+				return BadRequest();
 
-            var dbName = PreviewMode + (PreviewMode == "tenant" ? TenantId : AppId);
+			var dbName = PreviewMode + (PreviewMode == "tenant" ? TenantId : AppId);
 
-            var path = DataHelper.GetDataDirectoryPath(_configuration, _hostingEnvironment);
-            var text = "";
+			var path = DataHelper.GetDataDirectoryPath(_configuration, _hostingEnvironment);
+			var text = "";
 
-            if (!System.IO.File.Exists(Path.Combine(path, "packages", dbName, package.Version, "log.txt")))
-                return Ok("Your logs have been deleted...");
+			if (!System.IO.File.Exists(Path.Combine(path, "packages", dbName, package.Version, "log.txt")))
+				return Ok("Your logs have been deleted...");
 
-            using (var fs = new FileStream(Path.Combine(path, "packages", dbName, package.Version, "log.txt"), FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
-            using (var sr = new StreamReader(fs, Encoding.Default))
-            {
-                text = ConvertHelper.ASCIIToHTML(sr.ReadToEnd());
-                sr.Close();
-                fs.Close();
-            }
+			using (var fs = new FileStream(Path.Combine(path, "packages", dbName, package.Version, "log.txt"), FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+			using (var sr = new StreamReader(fs, Encoding.Default))
+			{
+				text = ConvertHelper.ASCIIToHTML(sr.ReadToEnd());
+				sr.Close();
+				fs.Close();
+			}
 
-            return Ok(text);
-        }
+			return Ok(text);
+		}
 
-        [Route("count"), HttpGet]
-        public async Task<IActionResult> Count()
-        {
-            if (UserProfile != ProfileEnum.Manager && !_permissionHelper.CheckUserProfile(UserProfile, "package", RequestTypeEnum.View))
-                return StatusCode(403);
+		[Route("count"), HttpGet]
+		public async Task<IActionResult> Count()
+		{
+			if (UserProfile != ProfileEnum.Manager && !_permissionHelper.CheckUserProfile(UserProfile, "package", RequestTypeEnum.View))
+				return StatusCode(403);
 
-            var count = await _packageRepository.Count((int)AppId);
+			var count = await _packageRepository.Count((int)AppId);
 
-            return Ok(count);
-        }
+			return Ok(count);
+		}
 
-        [Route("find")]
-        public IActionResult Find(ODataQueryOptions<Package> queryOptions)
-        {
-            if (UserProfile != ProfileEnum.Manager && !_permissionHelper.CheckUserProfile(UserProfile, "package", RequestTypeEnum.View))
-                return StatusCode(403);
+		[Route("find")]
+		public IActionResult Find(ODataQueryOptions<Package> queryOptions)
+		{
+			if (UserProfile != ProfileEnum.Manager && !_permissionHelper.CheckUserProfile(UserProfile, "package", RequestTypeEnum.View))
+				return StatusCode(403);
 
-            var packages = _packageRepository.Find((int)AppId);
+			var packages = _packageRepository.Find((int)AppId);
 
-            var queryResults = (IQueryable<Package>)queryOptions.ApplyTo(packages, new ODataQuerySettings() { EnsureStableOrdering = false });
-            return Ok(new PageResult<Package>(queryResults, Request.ODataFeature().NextLink, Request.ODataFeature().TotalCount));
-        }
+			var queryResults = (IQueryable<Package>)queryOptions.ApplyTo(packages, new ODataQuerySettings() { EnsureStableOrdering = false });
+			return Ok(new PageResult<Package>(queryResults, Request.ODataFeature().NextLink, Request.ODataFeature().TotalCount));
+		}
 
-        [Route("get/{id}"), HttpGet]
-        public async Task<IActionResult> Get(int id)
-        {
-            if (UserProfile != ProfileEnum.Manager && !_permissionHelper.CheckUserProfile(UserProfile, "package", RequestTypeEnum.View))
-                return StatusCode(403);
+		[Route("get/{id}"), HttpGet]
+		public async Task<IActionResult> Get(int id)
+		{
+			if (UserProfile != ProfileEnum.Manager && !_permissionHelper.CheckUserProfile(UserProfile, "package", RequestTypeEnum.View))
+				return StatusCode(403);
 
-            var package = await _packageRepository.Get(id);
+			var package = await _packageRepository.Get(id);
 
-            if (package == null)
-                return BadRequest();
+			if (package == null)
+				return BadRequest();
 
-            return Ok(package);
-        }
+			return Ok(package);
+		}
 
-        [Route("get_all/{appId}"), HttpGet]
-        public async Task<IActionResult> GetAll(int appId)
-        {
-            if (UserProfile != ProfileEnum.Manager && !_permissionHelper.CheckUserProfile(UserProfile, "package", RequestTypeEnum.View))
-                return StatusCode(403);
+		[Route("get_all/{appId}"), HttpGet]
+		public async Task<IActionResult> GetAll(int appId)
+		{
+			if (UserProfile != ProfileEnum.Manager && !_permissionHelper.CheckUserProfile(UserProfile, "package", RequestTypeEnum.View))
+				return StatusCode(403);
 
-            var packages = await _packageRepository.GetAll(appId);
+			var packages = await _packageRepository.GetAll(appId);
 
-            if (packages.Count <= 0)
-                return NotFound();
+			if (packages.Count <= 0)
+				return NotFound();
 
-            return Ok(packages);
-        }
+			return Ok(packages);
+		}
 
-        [HttpPost]
-        [Route("create")]
-        public async Task<IActionResult> Create([FromBody]JObject model)
-        {
-            if (!_permissionHelper.CheckUserProfile(UserProfile, "package", RequestTypeEnum.Create))
-                return StatusCode(403);
+		[HttpPost]
+		[Route("create")]
+		public async Task<IActionResult> Create([FromBody]JObject model)
+		{
+			if (!_permissionHelper.CheckUserProfile(UserProfile, "package", RequestTypeEnum.Create))
+				return StatusCode(403);
 
-            var anyProcess = await _packageRepository.GetActiveProcess((int)AppId);
+			var anyProcess = await _packageRepository.GetActiveProcess((int)AppId);
 
-            if (anyProcess != null)
-                return Conflict("A packaging process is currently in progress.");
+			if (anyProcess != null)
+				return Conflict("A packaging process is currently in progress.");
 
-            var dbHistory = await _historyDatabaseRepository.GetLast();
+			var dbHistory = await _historyDatabaseRepository.GetLast();
 
-            if (dbHistory?.Tag != null)
-                return Conflict("Please modify your app to create a new package.");
+			if (dbHistory?.Tag != null)
+				return Conflict("Please modify your app to create a new package.");
 
-            var dbName = PreviewMode + (PreviewMode == "tenant" ? TenantId : AppId);
+			var dbName = PreviewMode + (PreviewMode == "tenant" ? TenantId : AppId);
 
-            var currentBuildNumber = await _packageRepository.Count((int)AppId);
-            var version = currentBuildNumber + 1;
+			var currentBuildNumber = await _packageRepository.Count((int)AppId);
+			var version = currentBuildNumber + 1;
 
-            var app = await _appDraftRepository.Get((int)AppId);
-            var appOptions = new JObject { ["clear_all_records"] = true, ["enable_registration"] = true };
+			var app = await _appDraftRepository.Get((int)AppId);
+			var appOptions = new JObject { ["enable_registration"] = true };
 
-            if (app.Setting != null && !string.IsNullOrEmpty(app.Setting.Options))
-                appOptions = JObject.Parse(app.Setting.Options);
+			if (app.Setting != null && !string.IsNullOrEmpty(app.Setting.Options))
+				appOptions = JObject.Parse(app.Setting.Options);
 
-            var packageModel = new Package()
-            {
-                AppId = (int)AppId,
-                Version = version.ToString(),
-                Revision = version,
-                StartTime = DateTime.Now,
-                Status = ReleaseStatus.Running,
-                Settings = new JObject
-                {
-                    ["clear_all_records"] = appOptions["clear_all_records"],
-                    ["enable_registration"] = appOptions["enable_registration"]
-                }.ToString()
-            };
+			var packageModel = new Package()
+			{
+				AppId = (int)AppId,
+				Version = version.ToString(),
+				Revision = version,
+				StartTime = DateTime.Now,
+				Status = ReleaseStatus.Running,
+				Settings = new JObject
+				{
+					["protect_modules"] = model["protectModules"],
+					["selected_modules"] = model["selectedModules"],
+					["modules_relations"] = model["modulesRelations"],
+					["enable_registration"] = appOptions["enable_registration"]
+				}.ToString()
+			};
 
-            await _packageRepository.Create(packageModel);
+			await _packageRepository.Create(packageModel);
 
-            /*
+			/*
              * Set version number to history_database and history_storage tables tag column.
              */
 
-            if (dbHistory != null && dbHistory.Tag == null)
-            {
-                dbHistory.Tag = packageModel.Version;
-                await _historyDatabaseRepository.Update(dbHistory);
-            }
+			if (dbHistory != null && dbHistory.Tag == null)
+			{
+				dbHistory.Tag = packageModel.Version;
+				await _historyDatabaseRepository.Update(dbHistory);
+			}
 
-            var storageHistory = await _historyStorageRepository.GetLast();
-            if (storageHistory != null && storageHistory.Tag == null)
-            {
-                storageHistory.Tag = packageModel.Version;
-                await _historyStorageRepository.Update(storageHistory);
-            }
+			var storageHistory = await _historyStorageRepository.GetLast();
+			if (storageHistory != null && storageHistory.Tag == null)
+			{
+				storageHistory.Tag = packageModel.Version;
+				await _historyStorageRepository.Update(storageHistory);
+			}
 
-            if (await _packageRepository.IsFirstPackage((int)AppId))
-            {
-                var historyStorages = await _historyStorageRepository.GetAll();
-                _queue.QueueBackgroundWorkItem(token => _packageHelper.All((int)AppId, (bool)appOptions["clear_all_records"], dbName, version.ToString(), packageModel.Id, historyStorages));
-            }
-            else
-            {
-                List<HistoryDatabase> historyDatabase = null;
-                List<HistoryStorage> historyStorages = null;
+			if (await _packageRepository.IsFirstPackage((int)AppId))
+			{
+				var historyStorages = await _historyStorageRepository.GetAll();
+				_queue.QueueBackgroundWorkItem(token => _packageHelper.All((int)AppId, model, dbName, version.ToString(), packageModel.Id, historyStorages));
+			}
+			else
+			{
+				List<HistoryDatabase> historyDatabase = null;
+				List<HistoryStorage> historyStorages = null;
 
-                if (dbHistory != null && dbHistory.Tag != (int.Parse(packageModel.Version) - 1).ToString())
-                    historyDatabase = await _historyDatabaseRepository.GetDiffs(currentBuildNumber.ToString());
+				if (dbHistory != null && dbHistory.Tag != (int.Parse(packageModel.Version) - 1).ToString())
+					historyDatabase = await _historyDatabaseRepository.GetDiffs(currentBuildNumber.ToString());
 
-                if (storageHistory != null && storageHistory.Tag != (int.Parse(packageModel.Version) - 1).ToString())
-                    historyStorages = await _historyStorageRepository.GetDiffs(currentBuildNumber.ToString());
+				if (storageHistory != null && storageHistory.Tag != (int.Parse(packageModel.Version) - 1).ToString())
+					historyStorages = await _historyStorageRepository.GetDiffs(currentBuildNumber.ToString());
 
-                _queue.QueueBackgroundWorkItem(token => _packageHelper.Diffs(historyDatabase, historyStorages, (int)AppId, dbName, version.ToString(), packageModel.Id));
-            }
+				_queue.QueueBackgroundWorkItem(token => _packageHelper.Diffs(historyDatabase, historyStorages, (int)AppId, dbName, version.ToString(), packageModel.Id));
+			}
 
-            return Ok(packageModel.Id);
-        }
+			return Ok(packageModel.Id);
+		}
 
-        [HttpGet]
-        [Route("get_active_process")]
-        public async Task<IActionResult> GetActiveProcess()
-        {
-            if (!_permissionHelper.CheckUserProfile(UserProfile, "package", RequestTypeEnum.Create))
-                return StatusCode(403);
+		[HttpGet]
+		[Route("get_active_process")]
+		public async Task<IActionResult> GetActiveProcess()
+		{
+			if (!_permissionHelper.CheckUserProfile(UserProfile, "package", RequestTypeEnum.Create))
+				return StatusCode(403);
 
-            if (AppId == null)
-                return Ok();
+			if (AppId == null)
+				return Ok();
 
-            var process = await _packageRepository.GetActiveProcess((int)AppId);
+			var process = await _packageRepository.GetActiveProcess((int)AppId);
 
-            return Ok(process);
-        }
+			return Ok(process);
+		}
 
-        [HttpGet]
-        [Route("get_last")]
-        public async Task<IActionResult> GetLastPackage()
-        {
-            if (!_permissionHelper.CheckUserProfile(UserProfile, "package", RequestTypeEnum.Create))
-                return StatusCode(403);
+		[HttpGet]
+		[Route("get_last")]
+		public async Task<IActionResult> GetLastPackage()
+		{
+			if (!_permissionHelper.CheckUserProfile(UserProfile, "package", RequestTypeEnum.Create))
+				return StatusCode(403);
 
-            var result = await _packageRepository.GetLastPackage((int)AppId);
+			var result = await _packageRepository.GetLastPackage((int)AppId);
 
-            return Ok(result);
-        }
-    }
+			return Ok(result);
+		}
+	}
 }
