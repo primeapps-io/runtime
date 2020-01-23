3a421ed1f989b8c58f957e69c6d011a573c36694
Galip Çevrik <galip.cevrik@primeapps.io>
2020-01-23 13:59:24 +0300

    #3937 Global config'e eklenen, header'da gelmesi gereken X-Tenant-Language null geliyor

--- PrimeApps.App/Controllers/HomeController.cs
+++ PrimeApps.App/Controllers/HomeController.cs
@@ -245,10 +245,8 @@ namespace PrimeApps.App.Controllers
 
                     if (userInfo != null)
                     {
-                         hasAdminRight = userInfo.profile.HasAdminRights;
-                         tenantLanguage = userInfo.tenantLanguage;
-                    }
-                       
+                         hasAdminRight = userInfo.profile.HasAdminRights; 
+                    }   
                 }
                 
                 var platformDatabaseContext = _scope.ServiceProvider.GetRequiredService<PlatformDBContext>();
@@ -256,9 +254,11 @@ namespace PrimeApps.App.Controllers
                 {
                     platformUserRepository.CurrentUser = new CurrentUser { UserId = userId, TenantId = previewMode == "app" ? (int)appId : (int)tenantId, PreviewMode = previewMode };
                     var platformUser = await platformUserRepository.GetSettings(userId);
+
                     if (platformUser != null)
                     {
                         account["user"] = JObject.Parse(JsonConvert.SerializeObject(platformUser, serializerSettings));
+                        tenantLanguage = platformUser.Setting?.Language ?? "en";
                     }
                 }
             }
