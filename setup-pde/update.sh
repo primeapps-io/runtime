aa3fdb4c0fb7e0af54ddd85fae25dfe322ed608a
Fatih Sever <fatih.sever@primeapps.io>
2020-02-13 19:16:25 +0300

    #3564 migrate.sh completed and renamed as update.sh

---
+++ setup-pde/update.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+NC='\033[0m' # No Color
+
+# Variables
+os="linux"
+
+if [[ "$OSTYPE" == "msys" ]]
+then
+	os="win"
+elif [[ "$OSTYPE" == "darwin"* ]]
+then
+	os="osx"
+fi
+
+# Get parameters
+for i in "$@"
+do
+case $i in
+    -c=*|--connection-string=*)
+    connectionString="${i#*=}"
+    ;;
+    *)
+    # unknown option
+    ;;
+esac
+done
+
+# Publish Migrator
+echo -e "${GREEN}Publishing Migrator...${NC}"
+dotnet publish "PrimeApps.Migrator.csproj" --self-contained false --runtime "$os-x64" -c Debug
+
+# Run PRE migrate command
+cd bin/Debug/netcoreapp2.2/"$os-x64"/publish
+./migrator update-pde $connectionString
---
+++ setup/update.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+NC='\033[0m' # No Color
+
+# Variables
+os="linux"
+
+if [[ "$OSTYPE" == "msys" ]]
+then
+	os="win"
+elif [[ "$OSTYPE" == "darwin"* ]]
+then
+	os="osx"
+fi
+
+fileMigrator=${PRIMEAPPS_FILE_MIGRATOR:-"http://file.primeapps.io/pre/migrator-$os-x64.zip"}
+connectionString=""
+
+# Get parameters
+for i in "$@"
+do
+case $i in
+    -c=*|--connection-string=*)
+    connectionString="${i#*=}"
+    ;;
+    *)
+    # unknown option
+    ;;
+esac
+done
+
+# Remove migrator folder and migrator.zip 
+rm -rf migrator
+rm migrator.zip
+
+# Download Migrator
+echo -e "${GREEN}Downloading Migrator...${NC}"
+curl $fileMigrator -L --output migrator.zip
+unzip migrator.zip
+mv $os-x64/ migrator/
+rm migrator.zip
+
+# Run PRE migrate command
+cd migrator
+./migrator update-pre $connectionString
