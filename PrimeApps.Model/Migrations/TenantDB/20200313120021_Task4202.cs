f2493898e62accacea882afbf003888467055d02
Galip Çevrik <galip.cevrik@primeapps.io>
2020-03-13 15:01:52 +0300

    #4202 Menu profile_id alanının geri eklenmesi.

---
+++ PrimeApps.Model/Migrations/TenantDB/20200313120021_Task4202.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PrimeApps.Model.Migrations.TenantDB
+{
+    public partial class Task4202 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "profile_id",
+                schema: "public",
+                table: "menu",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_menu_profile_id",
+                schema: "public",
+                table: "menu",
+                column: "profile_id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_menu_profiles_profile_id",
+                schema: "public",
+                table: "menu",
+                column: "profile_id",
+                principalSchema: "public",
+                principalTable: "profiles",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_menu_profiles_profile_id",
+                schema: "public",
+                table: "menu");
+
+            migrationBuilder.DropIndex(
+                name: "IX_menu_profile_id",
+                schema: "public",
+                table: "menu");
+
+            migrationBuilder.DropColumn(
+                name: "profile_id",
+                schema: "public",
+                table: "menu");
+        }
+    }
+}
