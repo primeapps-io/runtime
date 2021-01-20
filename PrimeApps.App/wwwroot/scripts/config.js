40857aeebdd104eafbbbae6dfdda173c9932b41a
PrimeApps <admin@primeapps.io>
2021-01-20 10:08:09 +0000

    Updates and bugfixes

--- PrimeApps.App/wwwroot/scripts/config.js
+++ PrimeApps.App/wwwroot/scripts/config.js
@@ -1,13 +1,1 @@
-'use strict';
-
-angular.module('primeapps')
-
-    .constant('config', {
-        apiUrl: 'api/',
-        clientId: '00000000000000000000000000000000',
-        planIdMembers: '7673E999-18FB-497F-A958-84DCA43031CC',
-        planIdUsers: 'F89E4FBF-A50F-40BA-BBEC-FE027F3F1524'
-    })
-    .constant('blacklist', {
-    passwords: []
-});
+"use strict";angular.module("primeapps").constant("config",{apiUrl:"api/",clientId:"00000000000000000000000000000000",planIdMembers:"7673E999-18FB-497F-A958-84DCA43031CC",planIdUsers:"F89E4FBF-A50F-40BA-BBEC-FE027F3F1524"}).constant("blacklist",{passwords:[]});
--- PrimeApps.App/wwwroot/view/setup/organization/organizationService.js
+++ PrimeApps.App/wwwroot/view/setup/organization/organizationService.js
@@ -1,14 +1,1 @@
-'use strict';
-
-angular.module('primeapps')
-    .factory('OrganizationService', ['$http', 'config',
-        function ($http, config) {
-            return {
-
-                editCompany: function (company) {
-                    return $http.post(config.apiUrl + 'settings/edit', company);
-                }
-
-            };
-        }
-    ]);
+"use strict";angular.module("primeapps").factory("OrganizationService",["$http","config",function(t,i){return{editCompany:function(n){return t.post(i.apiUrl+"settings/edit",n)}}}]);
